ca601609e4047747da9e2a0873ad336d0b620887
lDucks <lducksusa@gmail.com>
2014-02-15 16:28:35 -0500

    Update deploy script

--- deploy.sh
+++ deploy.sh
@@ -5,6 +5,4 @@ wait
 echo "Git repository pulled - running compression scripts"
 java -jar /home/tools/compiler.jar --js /home/battleplugins/git/BattlePlugins/laravel/public/assets/js/scripts.js --js_output_file /home/battleplugins/git/BattlePlugins/laravel/public/assets/js/scripts.min.js
 java -jar /home/tools/compiler.jar --js /home/battleplugins/git/BattlePlugins/laravel/public/assets/js/admin.js --js_output_file /home/battleplugins/git/BattlePlugins/laravel/public/assets/js/admin.min.js
-chown -R lducks:deploy /home/battleplugins
-chmod -R 774 /home/battleplugins
 echo "Done"

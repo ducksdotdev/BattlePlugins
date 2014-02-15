37c1c16e61342fc95cdcaa8c4bde1c6b14fc70d7
lDucks <lducksusa@gmail.com>
2014-02-15 13:13:36 -0500

    Update deploy script

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,7 @@
 cd /home/battleplugins/git/BattlePlugins
+git stash
 git pull
 echo "Git repository pulled - running compression scripts"
-java -jar /home/tools/compiler.jar --js /home/battleplugins/website/laravel/public/assets/js/scripts.js --js_output_file /home/battleplugins/website/laravel/public/assets/js/scripts.min.js
-java -jar /home/tools/compiler.jar --js /home/battleplugins/website/laravel/public/assets/js/admin.js --js_output_file /home/battleplugins/website/laravel/public/assets/js/admin.min.js
+java -jar /home/tools/compiler.jar --js /home/battleplugins/git/BattlePlugins/laravel/public/assets/js/scripts.js --js_output_file /home/battleplugins/git/BattlePlugins/laravel/public/assets/js/scripts.min.js
+java -jar /home/tools/compiler.jar --js /home/battleplugins/git/BattlePlugins/laravel/public/assets/js/admin.js --js_output_file /home/battleplugins/git/BattlePlugins/laravel/public/assets/js/admin.min.js
 echo "Done"

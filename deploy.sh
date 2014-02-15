c957c42e1c8ac7067c08bc1cfbecba722c550abd
lDucks <lducksusa@gmail.com>
2014-02-15 18:37:30 -0500

    Comment out compiler

--- deploy.sh
+++ deploy.sh
@@ -1,8 +1,6 @@
 git stash
 wait
 git pull origin master
-wait
-echo "Git repository pulled - running compression scripts"
-java -jar /home/tools/compiler.jar --js /home/battleplugins/git/BattlePlugins/laravel/public/assets/js/scripts.js --js_output_file /home/battleplugins/git/BattlePlugins/laravel/public/assets/js/scripts.min.js
-java -jar /home/tools/compiler.jar --js /home/battleplugins/git/BattlePlugins/laravel/public/assets/js/admin.js --js_output_file /home/battleplugins/git/BattlePlugins/laravel/public/assets/js/admin.min.js
-echo "Done"
+#wait
+# java -jar /home/tools/compiler.jar --js /home/battleplugins/git/BattlePlugins/laravel/public/assets/js/scripts.js --js_output_file /home/battleplugins/git/BattlePlugins/laravel/public/assets/js/scripts.min.js
+#java -jar /home/tools/compiler.jar --js /home/battleplugins/git/BattlePlugins/laravel/public/assets/js/admin.js --js_output_file /home/battleplugins/git/BattlePlugins/laravel/public/assets/js/admin.min.js

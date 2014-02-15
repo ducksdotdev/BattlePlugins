ff69a840d93c4c3bfe70311f88eeee0038ce6273
lDucks <lducksusa@gmail.com>
2014-02-15 18:27:39 -0500

    Change deploy script to target specific branch

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,6 @@
 git stash
 wait
-git pull
+git pull origin master
 wait
 echo "Git repository pulled - running compression scripts"
 java -jar /home/tools/compiler.jar --js /home/battleplugins/git/BattlePlugins/laravel/public/assets/js/scripts.js --js_output_file /home/battleplugins/git/BattlePlugins/laravel/public/assets/js/scripts.min.js

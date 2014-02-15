6e2b631341dae3d7c1b74e87e3cb1fd6cf0ae15f
Justin <justin@battleplugins.com>
2014-02-15 12:11:40 -0500

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -3,3 +3,4 @@ git pull
 echo "Git repository pulled - running compression scripts"
 java -jar /home/tools/compiler.jar --js /home/battleplugins/website/laravel/public/assets/js/scripts.js --js_output_file /home/battleplugins/website/laravel/public/assets/js/scripts.min.js
 java -jar /home/tools/compiler.jar --js /home/battleplugins/website/laravel/public/assets/js/admin.js --js_output_file /home/battleplugins/website/laravel/public/assets/js/admin.min.js
+echo "Done"

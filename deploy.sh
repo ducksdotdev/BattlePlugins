9e4e1f0e394ee28eaa844a613b7bcf0611a2b2d7
lDucks <lducksusa@gmail.com>
2014-02-15 20:28:58 -0500

    Add CSS minifying

--- deploy.sh
+++ deploy.sh
@@ -5,3 +5,4 @@ git pull
 wait
 java -jar /home/tools/compiler.jar --js /home/battleplugins/git/BattlePlugins/laravel/public/assets/js/scripts.js --js_output_file /home/battleplugins/git/BattlePlugins/laravel/public/assets/js/scripts.min.js
 java -jar /home/tools/compiler.jar --js /home/battleplugins/git/BattlePlugins/laravel/public/assets/js/admin.js --js_output_file /home/battleplugins/git/BattlePlugins/laravel/public/assets/js/admin.min.js
+java -jar /home/tools/closure-stylesheets.jar /home/battleplugins/git/BattlePlugins/laravel/public/assets/css/style.css > style.min.css

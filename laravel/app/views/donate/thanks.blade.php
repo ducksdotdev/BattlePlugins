ad7b42b4921f287bea211095bba2142ce8650694
lDucks <lducksusa@gmail.com>
2014-02-15 23:21:05 -0500

    English

--- laravel/app/views/donate/thanks.blade.php
+++ laravel/app/views/donate/thanks.blade.php
@@ -13,7 +13,7 @@
     <div class="container">
         <div class="row">
             <div class="col-lg-12">
-                <p>Running BattlePlugins isn't free. Every little bit helps us keep this project funded and allows us to continue to expand. Every little bit helps and we really, really appreciate your kindness. BattlePlugins wouldn't be here today without our dedicated users.</p>
+                <p>Running BattlePlugins isn't free. Your donation will help us keep this project funded and allows us to continue to expand. Every little bit helps and we really, really appreciate your kindness. BattlePlugins wouldn't be here today without our dedicated users.</p>
                 <p>Please do not refresh this page, instead <a href="/">click here</a> to go back to the homepage.</p>
             </div>
         </div>

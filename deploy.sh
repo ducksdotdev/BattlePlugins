9d4653bf6263668ace09df075b09c178d9a424e8
lDucks <lducksusa@gmail.com>
2014-02-15 19:02:24 -0500

    Update deploy

--- deploy.sh
+++
@@ -1,9 +0,0 @@
-su lducks
-whoami
-wait
-git stash
-wait
-git pull origin master
-#wait
-# java -jar /home/tools/compiler.jar --js /home/battleplugins/git/BattlePlugins/laravel/public/assets/js/scripts.js --js_output_file /home/battleplugins/git/BattlePlugins/laravel/public/assets/js/scripts.min.js
-#java -jar /home/tools/compiler.jar --js /home/battleplugins/git/BattlePlugins/laravel/public/assets/js/admin.js --js_output_file /home/battleplugins/git/BattlePlugins/laravel/public/assets/js/admin.min.js

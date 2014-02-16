8fe3f4989aac6041f201266dc6ec005a412757d1
lDucks <lducksusa@gmail.com>
2014-02-15 19:44:16 -0500

    Update deploy

---
+++ deploy.sh
@@ -0,0 +1,6 @@
+git stash
+wait
+git pull
+wait
+java -jar /home/tools/compiler.jar --js /home/battleplugins/git/BattlePlugins/laravel/public/assets/js/scripts.js --js_output_file /home/battleplugins/git/BattlePlugins/laravel/public/assets/js/scripts.min.js
+java -jar /home/tools/compiler.jar --js /home/battleplugins/git/BattlePlugins/laravel/public/assets/js/admin.js --js_output_file /home/battleplugins/git/BattlePlugins/laravel/public/assets/js/admin.min.js

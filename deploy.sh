c7ef6f4f6a6cfb7c198e5ef86ab9385ced69426f
lDucks <lducksusa@gmail.com>
2014-02-15 20:01:44 -0500

    Testing deploy!

--- deploy.sh
+++ deploy.sh
@@ -3,5 +3,5 @@ git stash
 wait
 git pull
 wait
-java -jar /home/tools/compiler.jar --js /laravel/public/assets/js/scripts.js --js_output_file /laravel/public/assets/js/scripts.min.js
-java -jar /home/tools/compiler.jar --js /laravel/public/assets/js/admin.js --js_output_file /laravel/public/assets/js/admin.min.js
+java -jar /home/tools/compiler.jar --js /home/battleplugins/git/BattlePlugins/laravel/public/assets/js/scripts.js --js_output_file /home/battleplugins/git/BattlePlugins/laravel/public/assets/js/scripts.min.js
+java -jar /home/tools/compiler.jar --js /home/battleplugins/git/BattlePlugins/laravel/public/assets/js/admin.js --js_output_file /home/battleplugins/git/BattlePlugins/laravel/public/assets/js/admin.min.js

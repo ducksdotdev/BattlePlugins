9a0210acad1019448b198fac98feb3b518ab6ad3
lDucks <lducksusa@gmail.com>
2014-02-19 17:33:11 -0500

    Revert deploy

--- deploy.sh
+++ deploy.sh
@@ -4,6 +4,6 @@ git stash
 wait
 git pull
 wait
-java -jar /home/tools/compiler.jar --js laravel/public/assets/js/scripts.js --js_output_file laravel/public/assets/js/scripts.min.js
-java -jar /home/tools/compiler.jar --js laravel/public/assets/js/admin.js --js_output_file laravel/public/assets/js/admin.min.js
-java -jar /home/tools/closure-stylesheets.jar laravel/public/assets/css/style.css > laravel/public/assets/css/style.min.css
+java -jar /home/tools/compiler.jar --js /home/battleplugins/git/BattlePlugins/laravel/public/assets/js/scripts.js --js_output_file /home/battleplugins/git/BattlePlugins/laravel/public/assets/js/scripts.min.js
+java -jar /home/tools/compiler.jar --js /home/battleplugins/git/BattlePlugins/laravel/public/assets/js/admin.js --js_output_file /home/battleplugins/git/BattlePlugins/laravel/public/assets/js/admin.min.js
+java -jar /home/tools/closure-stylesheets.jar /home/battleplugins/git/BattlePlugins/laravel/public/assets/css/style.css > /home/battleplugins/git/BattlePlugins/laravel/public/assets/css/style.min.css

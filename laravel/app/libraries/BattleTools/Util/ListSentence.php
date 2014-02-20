df2ddcab0938ff08a3e9647f2577b83c18656911
lDucks <lducksusa@gmail.com>
2014-02-19 22:58:39 -0500

    Change deployment method

--- laravel/app/libraries/BattleTools/Util/ListSentence.php
+++ laravel/app/libraries/BattleTools/Util/ListSentence.php
@@ -14,4 +14,13 @@ class ListSentence {
             return self::lastReplace(", ", " $glue ", implode(", ", $array));
         }
     }
+
+    public static function startsWith($haystack, $needle){
+        return $needle === "" || strpos($haystack, $needle) === 0;
+    }
+
+    public static function endsWith($haystack, $needle){
+        return $needle === "" || substr($haystack, -strlen($needle)) === $needle;
+    }
+
 }

a7d16fe570f89621519d5c2271ecfb005fa020b5
lDucks <justin@battleplugins.com>
2014-02-20 00:03:17 -0500

    Test

--- laravel/app/config/packages/kmd/logviewer/config.php
+++ laravel/app/config/packages/kmd/logviewer/config.php
@@ -2,14 +2,23 @@
 
 return array(
 
-    'base_url' => 'logviewer',
-    'filters'  => array(
-        'global' => array('before' => 'auth.administrator'),
+    'base_url'   => 'logviewer',
+    'filters'    => array(
+        'global' => array(),
         'view'   => array(),
-        'delete' => array(),
+        'delete' => array()
     ),
+<<<<<<< Updated upstream
     'log_dirs' => array('app' => storage_path().'/logs'),
     'log_order' => 'desc',   // Change to 'desc' for the latest entries first
     'per_page' => 10,
     'view'     => 'logviewer::viewer',
+=======
+    'log_dirs'   => array('app' => storage_path().'/logs'),
+    'log_order'  => 'asc', // Change to 'desc' for the latest entries first
+    'per_page'   => 10,
+    'view'       => 'logviewer::viewer',
+    'p_view'     => 'pagination::slider'
+
+>>>>>>> Stashed changes
 );

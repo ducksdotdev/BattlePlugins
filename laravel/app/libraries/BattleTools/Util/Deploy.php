aed11c44d6075f322f8bd921863c9930a57789ae
lDucks <lducksusa@gmail.com>
2014-02-20 00:43:01 -0500

    Remove fake error

--- laravel/app/libraries/BattleTools/Util/Deploy.php
+++ laravel/app/libraries/BattleTools/Util/Deploy.php
@@ -34,7 +34,10 @@ class Deploy {
 
         while($process->isRunning()){}
 
-        $output[$command] = array('output' => $process->getOutput(), 'errors' => $process->getErrorOutput());
+        $gitError = $process->getErrorOutput();
+        if(ListSentence::startsWith($gitError, 'From GitHub:'))
+
+        $output[$command] = array('output' => $process->getOutput(), 'errors' => $gitError);
 
         if($branch == 'master'){
             $doMinify = array(

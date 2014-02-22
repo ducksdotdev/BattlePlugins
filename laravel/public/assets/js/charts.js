8830c6bb394bd416f1b7af537d0f1445c75e7d51
lDucks <lducksusa@gmail.com>
2014-02-21 23:40:20 -0500

    Change JS

--- laravel/public/assets/js/charts.js
+++ laravel/public/assets/js/charts.js
@@ -1,24 +1,25 @@
 $(function(){
-    google.load('visualization', '1.0', {'packages':['corechart']});
-
-    $.get('/statistics/getTotalServers', function(jsonData){
-        google.setOnLoadCallback(drawChart);
-        function drawChart() {
-            // Create the data table.
-            var data = new google.visualization.DataTable();
-            data.addColumn('string', 'Topping');
-            data.addColumn('number', 'Slices');
-            data.addRows([
-                ['Mushrooms', 3],
-                ['Onions', 1],
-                ['Olives', 1],
-                ['Zucchini', 1],
-                ['Pepperoni', 2]
-            ]);
-
-            // Instantiate and draw our chart, passing in some options.
-            var chart = new google.visualization.AreaChart(document.getElementById('serversGraph'));
-            chart.draw(data);
-        }
+    $.get('/statistics/getTotalServers', function(data){
+        drawChart(data, 'serversGraph');
     });
+
 })
+
+google.load('visualization', '1.0', {'packages':['corechart']});
+function drawChart(data, div) {
+    // Create the data table.
+    var data = new google.visualization.DataTable();
+    data.addColumn('string', 'Topping');
+    data.addColumn('number', 'Slices');
+    data.addRows([
+        ['Mushrooms', 3],
+        ['Onions', 1],
+        ['Olives', 1],
+        ['Zucchini', 1],
+        ['Pepperoni', 2]
+    ]);
+
+    // Instantiate and draw our chart, passing in some options.
+    var chart = new google.visualization.AreaChart(document.getElementById(div));
+    chart.draw(data);
+}

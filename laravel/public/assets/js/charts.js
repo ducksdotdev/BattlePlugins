6a82062e18deea65d253a2dd5ea1c809a92a56e4
lDucks <lducksusa@gmail.com>
2014-02-22 00:05:00 -0500

    Pluck

--- laravel/public/assets/js/charts.js
+++ laravel/public/assets/js/charts.js
@@ -10,18 +10,6 @@ $(function () {
             subtitle: {
                 text: ''
             },
-            xAxis: {
-                labels: {
-                    formatter: function() {
-                        return this.value; // clean, unformatted number for year
-                    }
-                }
-            },
-            yAxis: {
-                title: {
-                    text: 'Nuclear weapon states'
-                }
-            },
             tooltip: {
                 pointFormat: '<b>{point.y:,.0f} {series.name}</b>'
             },

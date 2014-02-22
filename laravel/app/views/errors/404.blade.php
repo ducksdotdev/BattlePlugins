0981235f0eb62490fbbd2bdf3ff22d2cbab02b16
lDucks <lducksusa@gmail.com>
2014-02-21 21:15:33 -0500

    Update deploy script

--- laravel/app/views/errors/404.blade.php
+++ laravel/app/views/errors/404.blade.php
@@ -1,14 +1,24 @@
-<title>BattlePlugins :: 404 Not Found</title>
-<style>
-    body { text-align: center; padding: 150px; }
-    h1 { font-size: 50px; }
-    body { font: 20px Helvetica, sans-serif; color: #333; }
-    article { display: block; text-align: left; width: 650px; margin: 0 auto; }
-    a { color: #428bca; text-decoration: none; }
-    a:hover { color: #2a6496; text-decoration: underline; }
-</style>
+<html>
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="author" content="lDucks">
+    <title>BattlePlugins :: 404 Not Found</title>
+    <link rel="icon" type="image/png" href="/assets/img/favicon.png" />
+    <style>
+        body { text-align: center; padding: 150px; }
+        h1 { font-size: 50px; }
+        body { font: 20px Helvetica, sans-serif; color: #333; }
+        article { display: block; text-align: left; width: 650px; margin: 0 auto; }
+        a { color: #428bca; text-decoration: none; }
+        a:hover { color: #2a6496; text-decoration: underline; }
+    </style>
+</head>
+<body>
 <h1>404 Not Found</h1>
 <div>
     <p>The page you are looking for was not found.</p>
     <p>Why not check out the <a href="http://battleplugins.com">homepage</a> instead.</p>
 </div>
+</body>
+</html>

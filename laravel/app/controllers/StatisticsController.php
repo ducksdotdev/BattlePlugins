07d029bc57f2268f0faf1574976a5dd1c29e60fb
lDucks <lducksusa@gmail.com>
2014-02-22 00:05:20 -0500

    Pluck then get

--- laravel/app/controllers/StatisticsController.php
+++ laravel/app/controllers/StatisticsController.php
@@ -102,7 +102,7 @@ class StatisticsController extends BaseController {
             where('key', 'players')->
             select(DB::raw('sum(value) as players'))->
             groupBy('inserted_on')->
-            pluck('players');
+            pluck('players')->get();
 
         $players = array(
             'name' => 'Players',
@@ -113,7 +113,7 @@ class StatisticsController extends BaseController {
             where('key', 'players')->
             select(DB::raw('count(*) as servers'))->
             groupBy('inserted_on')->
-            pluck('servers');
+            pluck('servers')->get();
 
         $players = array(
             'name' => 'servers',

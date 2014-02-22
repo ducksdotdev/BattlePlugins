40f5b1f2d5add30b10e9d1bf51bcb82c116f9fdf
lDucks <lducksusa@gmail.com>
2014-02-22 13:36:50 -0500

    Change variable

--- laravel/public/assets/js/charts.js
+++ laravel/public/assets/js/charts.js
@@ -63,7 +63,7 @@ $(function () {
     }, 'json');
 });
 
-Date.toUnixTimestamp = function(mysql_string)
+Date.toUnixTimestamp = function(s)
 {
     s = s.split(/[-A-Z :\.]/i);
     var d = new Date(Date.UTC(s[0], --s[1], s[2], s[3], s[4], s[5]));

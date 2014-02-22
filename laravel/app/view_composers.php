17933a22f7b6f6d2334fb5d89532a55e06af10f6
lDucks <lducksusa@gmail.com>
2014-02-21 22:46:58 -0500

    Add dev to statistics

--- laravel/app/view_composers.php
+++ laravel/app/view_composers.php
@@ -16,6 +16,6 @@ View::composer(array('partials.head', 'partials.scripts'), function($view){
     $view->with('admin', $admin);
 });
 
-View::composer(array('layouts.master'), function($view){
+View::composer(array('master.head', 'partials.scripts', 'partials.nav', 'statistics'), function($view){
     $view->with('dev', Deploy::isDeveloperMode());
 });

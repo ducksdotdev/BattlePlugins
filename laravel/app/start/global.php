a2eb6812b87abb4196e9087427527fc08909226a
lDucks <lducksusa@gmail.com>
2014-02-23 12:25:20 -0500

    Remove 404 header

--- laravel/app/start/global.php
+++ laravel/app/start/global.php
@@ -51,7 +51,7 @@ Log::useDailyFiles(storage_path().'/logs/'.$logFile);
 App::error(function(Exception $exception, $code)
 {
     if ($exception instanceof Symfony\Component\HttpKernel\Exception\NotFoundHttpException) {
-        return View::make('errors.404', array(''), 404);
+        return View::make('errors.404');
     }else{
         Log::error($exception);
     }

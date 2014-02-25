9c58950bf1e7e9bdbf81f360b1dd0d48bd71c613
Justin <justin@battleplugins.com>
2014-02-25 15:43:06 -0500

    Change styles

--- laravel/app/views/developer/managePlugins.blade.php
+++ laravel/app/views/developer/managePlugins.blade.php
@@ -5,7 +5,7 @@
 		<div class="row">
 			<div class="col-lg-12">
 				<h2>Manage Plugins</h2>
-				<p>Add, edit, or delete plugins from this page. You may only add Battle"Plugins" to this list. Plugins will go through an approval process before being added to the list. Adding a plugin to the website enables the following features:</p>
+				<p>Add, edit, or delete plugins from this page. You may only add Battle<em>Plugins</em> to this list. Plugins will go through an approval process before being added to the list. Adding a plugin to the website enables the following features:</p>
 				<ul>
 					<li>Jenkins tracking via the homepage and plugin list</li>
 					<li>Statistics tracking for plugins using <a href="https://github.com/alkarinv/BattlePluginsAPI" target="_blank">BattlePluginsAPI</a></li>

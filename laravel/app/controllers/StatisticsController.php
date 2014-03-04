01222dd9da0959d86e00fea058ad691158be2ba8
Justin <justin@battleplugins.com>
2014-03-04 17:32:57 -0500

    String

--- laravel/app/controllers/StatisticsController.php
+++ laravel/app/controllers/StatisticsController.php
@@ -38,7 +38,7 @@ class StatisticsController extends BaseController{
 		parent::setActive('Resources');
 
 		$vars['title'] = 'Statistics';
-		$vars['curTime'] = Carbon::now();
+		$vars['curTime'] = Carbon::now()->toDateString();
 
 		return View::make('statistics', $vars);
 	}

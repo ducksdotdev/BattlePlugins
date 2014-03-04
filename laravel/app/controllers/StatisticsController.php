830c275f173c28ab01648c2ba3f009cb5fd8d422
Justin <justin@battleplugins.com>
2014-03-04 12:48:47 -0500

    Update StatisticsController.php

--- laravel/app/controllers/StatisticsController.php
+++ laravel/app/controllers/StatisticsController.php
@@ -47,7 +47,7 @@ class StatisticsController extends BaseController{
 
 		$server = Session::get('serverIp');
 
-		$cache = Cache::get('statistics', array());
+		$cache = Cache::get('statistics', function(){ return array() });
 		$cache[] = array(
 			'keys'   => $keys,
 			'server' => $server,

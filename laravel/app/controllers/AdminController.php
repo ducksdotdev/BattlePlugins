e76e9d6b8a173b95a1c1f3e7f6eb72fa8c57ec7f
Justin <justin@battleplugins.com>
2014-03-05 18:07:38 -0500

    Return 0

--- laravel/app/controllers/AdminController.php
+++ laravel/app/controllers/AdminController.php
@@ -268,7 +268,7 @@ class AdminController extends BaseController{
 
 		$vars['usernames'] = $usernames;
 		$vars['newStatistics'] = Cache::get('newStatistics');
-		$vars['lastUpdate'] = Cache::get('lastUpdate');
+		$vars['lastUpdate'] = Cache::get('lastUpdate', function(){return 0;});
 
 		return View::make('admin.statistics', $vars);
 	}

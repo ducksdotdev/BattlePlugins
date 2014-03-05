6d88beb2e8f447ec28fde1b44df054cd441f2ce0
Justin <justin@battleplugins.com>
2014-03-05 10:23:38 -0500

    Run graph building in queue

--- laravel/app/controllers/StatisticsController.php
+++ laravel/app/controllers/StatisticsController.php
@@ -77,32 +77,7 @@ class StatisticsController extends BaseController{
 	}
 
 	public function getTotalServers(){
-		return Cache::get('getTotalServers', function (){
-			$table = DB::table('server_statistics')->
-				where('key', 'bPlayersOnline')->
-				where('inserted_on', '<', DateUtil::getTimeToThirty())->
-				select(DB::raw('inserted_on as timestamp'), DB::raw('count(*) as servers'),
-					DB::raw('sum(value) as players'))->
-				groupBy(DB::raw('2 * HOUR( inserted_on ) + FLOOR( MINUTE( inserted_on ) / 30 )'))->
-				orderBy('timestamp', 'desc')->
-				take(336)->get();
-
-			if(count($table) > 0){
-				if(DateUtil::getTimeToThirty() == $table[0]->timestamp){
-					array_shift($table);
-				}
-
-				$table = array_reverse($table);
-
-				$diff = DateUtil::getTimeToThirty()->addMinutes(30);
-
-				$json = Response::json($table);
-
-				Cache::put('getTotalServers', $json, $diff);
-
-				return $json;
-			}
-		});
+		return Queue::push('BattleTools\Queue\UpdateServerChart');
 	}
 
 	public function getPluginCount(){

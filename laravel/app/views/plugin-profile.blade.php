aa2ff1851d49c0aae531921ce1c59669ad047116
Justin <justin@battleplugins.com>
2014-03-13 17:10:35 -0400

    Div spacing

--- laravel/app/views/plugin-profile.blade.php
+++ laravel/app/views/plugin-profile.blade.php
@@ -6,11 +6,11 @@
 			<div class="col-lg-12">
 				<h2>{{ $plugin->name }} <small>Created by {{ $plugin->author }}</small></h2>
 			</div>
-			<div class="row">
-				<div class="col-lg-12">
-					<a href="{{ $lastBuild['bukkit'] }}">Bukkit Project Page <i class="fa fa-external-link pull-right"></i></a><br />
-					Last Successful Build: <a href="{{ $lastBuild['ci']['url'] }}">{{ $lastBuild['ci']['build'] }}</a><br />
-				</div>
+		</div>
+		<div class="row">
+			<div class="col-lg-12">
+				<a href="{{ $lastBuild['bukkit'] }}">Bukkit Project Page <i class="fa fa-external-link pull-right"></i></a><br />
+				Last Successful Build: <a href="{{ $lastBuild['ci']['url'] }}">{{ $lastBuild['ci']['build'] }}</a><br />
 			</div>
 		</div>
 	</div>

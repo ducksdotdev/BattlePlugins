5d0e3277051138e27d9440241c413ca224653f48
Justin <justin@battleplugins.com>
2014-03-12 20:42:21 -0400

    Change both times

--- laravel/app/controllers/StatisticsController.php
+++ laravel/app/controllers/StatisticsController.php
@@ -59,7 +59,7 @@ class StatisticsController extends BaseController{
 		return Cache::get('getTotalServers', function(){
 			$diff = Carbon::now()->diffInMinutes(DateUtil::getTimeToThirty()->addMinutes(30));
 
-			$table = DB::select('select count(distinct server) as nServers, sum(avg_players) as nPlayers, FROM_UNIXTIME(newTime*1800) as time from (select server,round(avg(bPlayersOnline)) as avg_players, inserted_on as timestamp, (FLOOR(UNIX_TIMESTAMP(innerTable.inserted_on)/1800)) as newTime from server_statistics as innerTable where innerTable.inserted_on<"'.DateUtil::getTimeToThirty().'" group by server, newTime) as st1 group by newTime order by time desc limit 336');
+			$table = DB::select('select count(distinct server) as nServers, sum(avg_players) as nPlayers, FROM_UNIXTIME(newTime*900) as time from (select server,round(avg(bPlayersOnline)) as avg_players, inserted_on as timestamp, (FLOOR(UNIX_TIMESTAMP(innerTable.inserted_on)/900)) as newTime from server_statistics as innerTable where innerTable.inserted_on<"'.DateUtil::getTimeToThirty().'" group by server, newTime) as st1 group by newTime order by time desc limit 336');
 
 			if(count($table) > 0 && DateUtil::getTimeToThirty() <= $table[0]->time){
 				array_shift($table);

ad3df7262454e77d4d3c84c6b5d777e734a72991
Justin <justin@battleplugins.com>
2014-03-13 17:13:18 -0400

    Version statistic graph placeholder

--- laravel/app/views/plugin-profile.blade.php
+++ laravel/app/views/plugin-profile.blade.php
@@ -8,8 +8,8 @@
 			</div>
 		</div>
 		<div class="row">
-			<div class="col-lg-12">
-				<a href="{{ $lastBuild['bukkit'] }}">Bukkit Project Page <i class="fa fa-external-link pull-right"></i></a><br />
+			<div class="col-lg-11 col-offset-lg-1">
+				<a href="{{ $lastBuild['bukkit'] }}">Bukkit Project Page <i class="fa fa-external-link"></i></a><br />
 				Last Successful Build: <a href="{{ $lastBuild['ci']['url'] }}">{{ $lastBuild['ci']['build'] }}</a><br />
 			</div>
 		</div>
@@ -17,7 +17,23 @@
 </div>
 <div class="content-section-b">
 	<div class="container">
-
+		<div class="row">
+			<div class="col-lg-12">
+				<div class="panel panel-default">
+					<div class="panel-heading">
+						<div class="panel-title"><i class="fa fa-bar-chart-o panel-icon"></i>Version Statistics</div>
+					</div>
+					<div id="versionStatistics" class="panel-body"></div>
+				</div>
+			</div>
+		</div>
 	</div>
 </div>
 @stop
+@section('extraScripts')
+<script src="/assets/js/highcharts.min.js"></script>
+@if($dev && !Config::get('deploy.minify-development'))
+<script src="/assets/js/charts.js"></script>
+@else
+<script src="/assets/js/charts.min.js"></script>
+@endif

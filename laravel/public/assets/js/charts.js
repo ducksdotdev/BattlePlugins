affa87a74bc8ebae6021a8483961c5f62f28c9a0
Justin <justin@battleplugins.com>
2014-03-14 15:20:46 -0400

    Log data

--- laravel/public/assets/js/charts.js
+++ laravel/public/assets/js/charts.js
@@ -173,6 +173,7 @@ $(function () {
         $.get('/statistics/plugin/'+plugin+'/version', function(data){
             data = JSON.stringify(data);
             data = data.replace(/"(\w+)"\s*:/g, '$1:');
+            console.log(data);
             $('#versionStatistics').highcharts({
                 chart: {
                     type: 'area',

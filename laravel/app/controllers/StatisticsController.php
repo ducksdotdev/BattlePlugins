f700a3ea78d514949f5d5d2e5efecda5531a0419
Justin <justin@battleplugins.com>
2014-03-14 14:40:15 -0400

    Change arrays

--- laravel/app/controllers/StatisticsController.php
+++ laravel/app/controllers/StatisticsController.php
@@ -119,7 +119,7 @@ class StatisticsController extends BaseController{
 
 				$data = array();
 				foreach($pluginStatistics as $stat){
-					$data[$stat->version][] = array($stat->timestamp, $stat->count);
+					$data[] = array('name'=>$stat->version, 'data'=>$stat->timestamp, $stat->count);
 				}
 
 				return Response::json($data);

2f293c7bbaecd6b52bb921ec90d45351321bf6a4
Justin <justin@battleplugins.com>
2014-03-14 20:46:05 -0400

    Use new carbon

--- laravel/app/controllers/StatisticsController.php
+++ laravel/app/controllers/StatisticsController.php
@@ -109,7 +109,7 @@ class StatisticsController extends BaseController{
 
 					$data = array();
 					foreach($pluginStatistics as $stat){
-						$data[$stat->version][] = array(Carbon::createFromTimestamp($stat->time)->getTimestamp(), intval($stat->count));
+						$data[$stat->version][] = array((new Carbon($stat->time))->getTimestamp(), intval($stat->count));
 					}
 
 					$sendData = array();

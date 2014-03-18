0c028ce6575422550ade0b0370a7048862ef920c
Justin <justin@battleplugins.com>
2014-03-18 16:43:29 -0400

    Logging

--- laravel/app/controllers/PluginController.php
+++ laravel/app/controllers/PluginController.php
@@ -54,6 +54,7 @@ class PluginController extends BaseController{
 				$pluginStats = Plugins::getVersionStatistics($plugin->name);
 				if(count($pluginStats) == 0){
 					$untracked[] = $plugin->name;
+					Log::info($plugin->name);
 				}
 			}
 

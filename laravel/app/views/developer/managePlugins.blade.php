d9f6dd3724bf1e7d94629858b71fefe68918cff0
Justin <justin@battleplugins.com>
2014-03-17 20:30:26 -0400

    Change to labels

--- laravel/app/views/developer/managePlugins.blade.php
+++ laravel/app/views/developer/managePlugins.blade.php
@@ -22,9 +22,15 @@
 			<div class="col-lg-12">
 				<div class="panel panel-default">
 					<div class="panel-body">
-						<div class="row">
-							<div class="col-lg-4"><h3>{{ $plugin->name }}</h3></div>
-							<div class="col-lg-8"><input type="text" class="form-control input-lg" placeholder="Bukkit URL" value="{{ $plugin->bukkit }}" /></div>
+						<div class="row form-horizontal">
+							<form class="form-horizontal" role="form">
+								<div class="form-group">
+									<label for="{{ $plugin->name }}" class="col-lg-4 control-label"><h3>{{ $plugin->name }}</h3></label>
+									<div class="col-lg-8">
+										<input type="text" class="form-control input-lg" id="{{ $plugin->name }}" placeholder="Bukkit URL">
+									</div>
+								</div>
+							</form>
 						</div>
 					</div>
 				</div>

6a7d3d8c50d34004d921d6de546b49d022fc0812
Justin <justin@battleplugins.com>
2014-03-18 09:03:21 -0400

    Remove null check

--- laravel/app/libraries/BattleTools/API/ProjectInfo.php
+++ laravel/app/libraries/BattleTools/API/ProjectInfo.php
@@ -3,12 +3,8 @@ namespace BattleTools\API;
 
 class ProjectInfo {
 	public static function getProjectInfo($slug) {
-		$project = array();
-
-		if(strpos($slug, ' ') !== false){
-			$project = file_get_contents("https://api.curseforge.com/servermods/projects?search=".$slug);
-			$project = json_decode($project);
-		}
+		$project = file_get_contents("https://api.curseforge.com/servermods/projects?search=".$slug);
+		$project = json_decode($project);
 
 		return $project;
 	}

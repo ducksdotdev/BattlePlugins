e96a8ce5c5b7c1f7ce28df2de03fefc1957612b3
Justin <justin@battleplugins.com>
2014-03-18 09:06:17 -0400

    Cache projects to send less requests to Curse

--- laravel/app/libraries/BattleTools/API/ProjectInfo.php
+++ laravel/app/libraries/BattleTools/API/ProjectInfo.php
@@ -1,11 +1,15 @@
 <?php
 namespace BattleTools\API;
 
-class ProjectInfo {
-	public static function getProjectInfo($slug) {
-		$project = file_get_contents("https://api.curseforge.com/servermods/projects?search=".$slug);
-		$project = json_decode($project);
+class ProjectInfo{
+	public static function getProjectInfo($slug){
+		return Cache::get("project.".$slug, function () use ($slug){
+			$project = file_get_contents("https://api.curseforge.com/servermods/projects?search=".$slug);
+			$project = json_decode($project);
 
-		return $project;
+			Cache::put("project.".$slug, $project, Carbon::now()->addWeek());
+
+			return $project;
+		});
 	}
 }

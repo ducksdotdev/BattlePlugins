28c015d8c94b739c7a92806e8eff55feca545130
Justin <justin@battleplugins.com>
2014-03-17 21:18:40 -0400

    Ugly form

--- laravel/app/views/ajax/addPluginForm.blade.php
+++ laravel/app/views/ajax/addPluginForm.blade.php
@@ -3,7 +3,7 @@
 		<div class="panel-body">
 			<div id="alert"></div>
 			<div class="row">
-				{{ Form::open(array('id'=>'addPluginForm')) }}
+				{{ Form::open(array('id'=>'addPluginForm','class'=>'ugly')) }}
 				<div class="col-lg-4"><h1 id="pluginName"></h1></div>
 				<div class="col-lg-7"><input type="text" class="form-control input-xl" placeholder="Plugin Slug" /></div>
 				<div class="col-lg-1"><button type="submit" class="btn btn-primary">Add</button></div>

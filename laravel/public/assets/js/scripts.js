3092532ec48bcf86373258d8f3053f590ff44e95
Justin <justin@battleplugins.com>
2014-03-18 09:32:13 -0400

    Check in JS too

--- laravel/public/assets/js/scripts.js
+++ laravel/public/assets/js/scripts.js
@@ -159,7 +159,7 @@ $(function () {
                 $("#plugins").prepend(data);
                 $("#pluginSlug").keyup(function(){
                     var slug = $(this).val();
-                    if(slug.length > 0){
+                    if(slug.length > 0 && /^[a-zA-Z0-9- ]*$/.test(str) !== false){
                         $.getJSON("/api/curse/project/"+slug, function(data){
                             if(data.result != "error"){
                                 $("#pluginName").empty().append('<a href="http://dev.bukkit.org/bukkit-plugins/'+data.slug+'" target="_blank">'+data.name+'</a>');

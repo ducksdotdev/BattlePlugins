9f903b079bf0109b87f2cdf41a2de62a6b0f1ab0
Justin <justin@battleplugins.com>
2014-03-18 08:52:29 -0400

    Don't check when spaces are provided

--- laravel/app/libraries/BattleTools/API/ProjectInfo.php
+++ laravel/app/libraries/BattleTools/API/ProjectInfo.php
@@ -3,8 +3,12 @@ namespace BattleTools\API;
 
 class ProjectInfo {
 	public static function getProjectInfo($slug) {
-		$project = file_get_contents("https://api.curseforge.com/servermods/projects?search=".$slug);
-		$project = json_decode($project);
+		$project = array();
+
+		if(strpos($slug, ' ') == -1){
+			$project = file_get_contents("https://api.curseforge.com/servermods/projects?search=".$slug);
+			$project = json_decode($project);
+		}
 
 		return $project;
 	}
--- laravel/app/views/ajax/addPluginForm.blade.php
+++ laravel/app/views/ajax/addPluginForm.blade.php
@@ -1,13 +1,15 @@
-<div class="row">
-	<div class="panel panel-primary" id="addPluginPanel">
-		<div class="panel-body">
-			<div id="alert"></div>
-			<div class="row">
-				{{ Form::open(array('id'=>'addPluginForm','class'=>'ugly')) }}
-				<div class="col-lg-4"><h1 id="pluginName"></h1></div>
-				<div class="col-lg-7"><div class="form-group" id="inputGroup"><input type="text" class="form-control input-xl" placeholder="Plugin Slug" name="pluginSlug" id="pluginSlug" autocomplete="off" /></div></div>
-				<div class="col-lg-1"><button type="submit" class="btn btn-primary" disabled>Add</button></div>
-				{{ Form::close() }}
+<div class="col-lg-12">
+	<div class="row">
+		<div class="panel panel-primary" id="addPluginPanel">
+			<div class="panel-body">
+				<div id="alert"></div>
+				<div class="row">
+					{{ Form::open(array('id'=>'addPluginForm','class'=>'ugly')) }}
+					<div class="col-lg-4"><h1 id="pluginName"></h1></div>
+					<div class="col-lg-7"><div class="form-group" id="inputGroup"><input type="text" class="form-control input-xl" placeholder="Plugin Slug" name="pluginSlug" id="pluginSlug" autocomplete="off" /></div></div>
+					<div class="col-lg-1"><button type="submit" class="btn btn-primary" disabled>Add</button></div>
+					{{ Form::close() }}
+				</div>
 			</div>
 		</div>
 	</div>

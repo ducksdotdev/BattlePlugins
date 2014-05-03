1765711d6000106e37022578da1e2ea3412fced5
Justin <justin@battleplugins.com>
2014-05-02 21:54:09 -0400

    Add cache back

--- laravel/app/libraries/BattleTools/Statistics/Servers.php
+++ laravel/app/libraries/BattleTools/Statistics/Servers.php
@@ -15,7 +15,7 @@ class Servers {
 		$diff = Carbon::now()->diffInMinutes(DateUtil::getTimeToThirty()->addMinutes($interval));
 		$path = Config::get('statistics.location')."/totals.json";
 
-//		return Cache::get('getTotalServers', function () use ($interval, $diff, $path){
+		return Cache::get('getTotalServers', function () use ($interval, $diff, $path){
 			$table = DB::select('select count(distinct server) as nServers, sum(avg_players) as nPlayers, FROM_UNIXTIME(newTime*'.($interval*60).') as time from (select server,round(avg(bPlayersOnline)) as avg_players, inserted_on as timestamp, (FLOOR(UNIX_TIMESTAMP(innerTable.inserted_on)/'.($interval*60).')) as newTime from server_statistics as innerTable where innerTable.inserted_on<"'.DateUtil::getTimeToThirty().'" group by server, newTime) as st1 group by newTime order by time');
 
 			$stats = json_decode(file_get_contents($path));
@@ -25,7 +25,7 @@ class Servers {
 			Cache::put('getTotalServers', $stats, $diff);
 
 			return $stats;
-//		});
+		});
 	}
 
 	public static function getAuthenticationModes(){

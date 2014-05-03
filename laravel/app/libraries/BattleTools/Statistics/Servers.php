7fc410be89b6ca2b44fc88f25e4a42194f88513e
Justin <justin@battleplugins.com>
2014-05-02 21:48:47 -0400

    Fix concat

--- laravel/app/libraries/BattleTools/Statistics/Servers.php
+++ laravel/app/libraries/BattleTools/Statistics/Servers.php
@@ -13,7 +13,7 @@ class Servers {
 	public static function getTotals(){
 		$interval = Config::get('statistics.interval');
 		$diff = Carbon::now()->diffInMinutes(DateUtil::getTimeToThirty()->addMinutes($interval));
-		$path = Config::get('statistics.location')+"/totals.json";
+		$path = Config::get('statistics.location')."/totals.json";
 
 		return Cache::get('getTotalServers', function () use ($interval, $diff, $path){
 			$table = DB::select('select count(distinct server) as nServers, sum(avg_players) as nPlayers, FROM_UNIXTIME(newTime*'.($interval*60).') as time from (select server,round(avg(bPlayersOnline)) as avg_players, inserted_on as timestamp, (FLOOR(UNIX_TIMESTAMP(innerTable.inserted_on)/'.($interval*60).')) as newTime from server_statistics as innerTable where innerTable.inserted_on<"'.DateUtil::getTimeToThirty().'" group by server, newTime) as st1 group by newTime order by time');

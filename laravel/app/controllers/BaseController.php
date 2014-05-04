4c04e0a32c44b70a719c6aa384517197b13de87f
Justin <justin@battleplugins.com>
2014-05-04 13:15:56 -0400

    Update all files

--- laravel/app/controllers/BaseController.php
+++ laravel/app/controllers/BaseController.php
@@ -3,7 +3,7 @@
 use BattleTools\UI\NavigationItem;
 use BattleTools\UserManagement\UserGroups;
 use BattleTools\UserManagement\UserSettings;
-	use BattleTools\Util\Deploy;
+use Illuminate\Routing\Controller;
 
 	class BaseController extends Controller {
     private static $activeNavTitle = null;

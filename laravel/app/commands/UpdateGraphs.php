7dfa6af5900b34eececbc8c471388d1257d48dbf
Justin <justin@battleplugins.com>
2014-05-04 14:27:16 -0400

    Change plugin updating method

--- laravel/app/commands/UpdateGraphs.php
+++ laravel/app/commands/UpdateGraphs.php
@@ -41,6 +41,9 @@ class UpdateGraphs extends Command{
 		self::updateTotals();
 		self::updateAuthMode();
 		DB::table('server_statistics')->where('inserted_on', '<', DateUtil::getTimeToThirty())->delete();
+		self::updatePlugins();
+		self::updatePluginUsage();
+		DB::table('plugin_statistics')->where('inserted_on', '<', DateUtil::getTimeToThirty())->delete();
 	}
 
 	/**
@@ -68,11 +71,12 @@ class UpdateGraphs extends Command{
 
 		$table = DB::select('select count(distinct server) as nServers, sum(avg_players) as nPlayers, FROM_UNIXTIME(newTime*'.($interval*60).') as time from (select server,round(avg(bPlayersOnline)) as avg_players, inserted_on as timestamp, (FLOOR(UNIX_TIMESTAMP(innerTable.inserted_on)/'.($interval*60).')) as newTime from server_statistics as innerTable where innerTable.inserted_on<"'.DateUtil::getTimeToThirty().'" group by server, newTime) as st1 group by newTime order by time');
 
-		$stats = json_decode(file_get_contents($path));
-		$stats = is_array($stats) ? $stats = array_merge($stats, $table) : $table;
+		if(file_exists($path)) {
+			$table = array_merge(json_decode(file_get_contents($path)), $table);
+		}
 
-		file_put_contents($path, json_encode($stats));
-		Cache::put('getTotalServers', $stats, $diff);
+		file_put_contents($path, json_encode($table));
+		Cache::put('getTotalServers', $table, $diff);
 	}
 
 	private function updateAuthMode(){
@@ -92,4 +96,83 @@ class UpdateGraphs extends Command{
 		Cache::put('getAuthMode', $table, $diff);
 	}
 
+	private function updatePluginUsage(){
+		$path = Config::get('statistics.location')."/overall/pluginusage.json";
+
+		$interval = Config::get('statistics.interval');
+		$diff = Carbon::now()->diffInMinutes(DateUtil::getTimeToThirty()->addMinutes($interval));
+
+		$table = DB::table('plugin_statistics')->
+		where('inserted_on', '>', DateUtil::getTimeToThirty()->subMinutes($interval))->
+		where('inserted_on', '<', DateUtil::getTimeToThirty()->subMinute())->
+		select('plugin', DB::raw('count(distinct server) as total'))->
+		groupBy('plugin')->
+		remember($diff)->get();
+
+		file_put_contents($path, json_decode($table));
+
+		Cache::put('pluginUsage', $table, $diff);
+	}
+
+	private function updatePlugins() {
+		$plugins = DB::table('plugins')->get();
+		$path = Config::get('statistics.location')."/plugins";
+
+		foreach ($plugins as $plugin) {
+			$interval = Config::get('statistics.interval');
+			$diff = Carbon::now()->diffInMinutes(DateUtil::getTimeToThirty()->addMinutes($interval));
+
+			$pluginStatistics = DB::table('plugin_statistics')->
+			select(
+				DB::raw('count(distinct server) as count'),
+				DB::raw('(FLOOR(UNIX_TIMESTAMP(inserted_on)/' . ($interval * 60) . ')) as time'),
+				'version')->
+			where('plugin', $plugin)->
+			where('inserted_on', '<', DateUtil::getTimeToThirty())->
+			groupBy('version', 'time')->
+			orderBy('time')->
+			remember($diff)->get();
+
+			$times = array();
+			$data = array();
+			$versions = array();
+			$counts = array();
+			foreach ($pluginStatistics as $stat) {
+				$times[] = $stat->time;
+				$versions[] = $stat->version;
+				$counts[$stat->version][$stat->time] = intval($stat->count);
+			}
+
+			$times = array_unique($times);
+			$versions = array_unique($versions);
+
+			foreach ($versions as $version) { // Check every statistic
+				foreach ($times as $time) { // Loop through every time
+					if (!array_key_exists($time, $counts[$version])) { // If statistic doesn't already have data from the database
+						$data[$version][] = array($time, null); // Set the statistic to null
+					} else {
+						$data[$version][] = array($time, $counts[$version][$time]); // Or else set the statistic to the database value
+					}
+				}
+			}
+
+			$sendData = array();
+			foreach (array_keys($data) as $key) {
+				$thisData = array();
+				foreach ($data[$key] as $part) {
+					$thisData[] = array(Carbon::createFromTimestampUTC($part[0] * $interval * 60)->getTimestamp() * 1000, $part[1]);
+				}
+				$sendData[] = array('name' => array_search($data[$key], $data), 'data' => $thisData);
+			}
+
+			$file = $path.'/'.$plugin->name.'.json';
+
+			if(file_exists($file)){
+				$sendData = array_merge(json_decode(file_get_contents($file)), $sendData);
+			}
+
+			file_put_contents($file, json_encode($sendData), $diff);
+			Cache::put($plugin->name.'Statistics', $sendData, $diff);
+		}
+	}
 }

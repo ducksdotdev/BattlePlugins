eaca4de7dce0ec2d25975c620b52ea8e284e6e7f
Justin <justin@battleplugins.com>
2014-05-05 07:43:50 -0400

    Divide by 1000

--- laravel/app/commands/UpdateGraphs.php
+++ laravel/app/commands/UpdateGraphs.php
@@ -127,7 +127,7 @@ class UpdateGraphs extends Command{
 				$oldData = json_decode(file_get_contents($file));
 				foreach($oldData as $d){
 					foreach($d->data as $part){
-						$times[] = $part[0];
+						$times[] = $part[0]/1000;
 					}
 				}
 
@@ -151,7 +151,7 @@ class UpdateGraphs extends Command{
 			foreach (array_keys($data) as $key) {
 				$thisData = array();
 				foreach ($data[$key] as $part) {
-					$thisData[] = array(Carbon::createFromTimestampUTC($part[0] * $interval * 60)->getTimestamp() / 100, $part[1]);
+					$thisData[] = array(Carbon::createFromTimestampUTC($part[0] * $interval * 60)->getTimestamp() * 1000, $part[1]);
 				}
 				$sendData[] = array('name' => array_search($data[$key], $data), 'data' => $thisData);
 			}

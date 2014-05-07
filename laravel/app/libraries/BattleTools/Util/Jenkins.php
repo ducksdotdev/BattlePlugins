7606ff245545c0dfb9397fcac6cbe439294cdfd3
Justin <justin@battleplugins.com>
2014-05-07 15:51:29 -0400

    Add Requests library

--- laravel/app/libraries/BattleTools/Util/Jenkins.php
+++ laravel/app/libraries/BattleTools/Util/Jenkins.php
@@ -5,35 +5,32 @@ namespace BattleTools\Util;
 class Jenkins {
 
 	public static function getLatestBuild($url, $job){
-		return array(
-			'exists' => false
-		);
+		$request = Requests::get($url . "/job/" . $job . "/lastSuccessfulBuild/", array(), array(
+			'timeout' => '3'
+		));
 
-//		$ctx = stream_context_create(array(
-//				'http' => array(
-//					'timeout' => 100
-//				)
-//			)
-//		);
-//
-//		$contents = @file_get_contents($url . "/job/" . $job . "/lastSuccessfulBuild/", 0, $ctx);
-//		$matches = array();
-//		$match = preg_match('/\<title\>[A-Za-z0-9-_]+ \#([0-9]+) \[Jenkins\]\<\/title\>/i', $contents, $matches);
-//		if ($match == 0){
-//			$array = array(
-//				'exists' => false,
-//				'build' => 'None',
-//				'url' => '#'
-//			);
-//		}else{
-//			$buildnum = $matches[1];
-//			$array = array(
-//				'exists' => true,
-//				'build' => $buildnum,
-//				'url' => $url.'/job/'.$job.'/'.$buildnum
-//			);
-//		}
-//		return $array;
+		if($request->success) {
+			$matches = array();
+			$match = preg_match('/\<title\>[A-Za-z0-9-_]+ \#([0-9]+) \[Jenkins\]\<\/title\>/i', $contents, $matches);
+			if ($match == 0) {
+				$array = array(
+					'exists' => false,
+					'build' => 'None',
+					'url' => '#'
+				);
+			} else {
+				$buildnum = $matches[1];
+				$array = array(
+					'exists' => true,
+					'build' => $buildnum,
+					'url' => $url . '/job/' . $job . '/' . $buildnum
+				);
+			}
+			return $array;
+		}else{
+			Log::emergency("It looks like the CI server is down at $url");
+			return array('exists'=>false);
+		}
 	}
 
 }

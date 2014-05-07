0d982cccc5002d6c4db4f134fc93a104422152d7
Justin <justin@battleplugins.com>
2014-05-07 13:50:20 -0400

    Comment out

--- laravel/app/libraries/BattleTools/Util/Jenkins.php
+++ laravel/app/libraries/BattleTools/Util/Jenkins.php
@@ -9,31 +9,31 @@ class Jenkins {
 			'exists' => false
 		);
 
-		$ctx = stream_context_create(array(
-				'http' => array(
-					'timeout' => 100
-				)
-			)
-		);
-
-		$contents = @file_get_contents($url . "/job/" . $job . "/lastSuccessfulBuild/", 0, $ctx);
-		$matches = array();
-		$match = preg_match('/\<title\>[A-Za-z0-9-_]+ \#([0-9]+) \[Jenkins\]\<\/title\>/i', $contents, $matches);
-		if ($match == 0){
-			$array = array(
-				'exists' => false,
-				'build' => 'None',
-				'url' => '#'
-			);
-		}else{
-			$buildnum = $matches[1];
-			$array = array(
-				'exists' => true,
-				'build' => $buildnum,
-				'url' => $url.'/job/'.$job.'/'.$buildnum
-			);
-		}
-		return $array;
+//		$ctx = stream_context_create(array(
+//				'http' => array(
+//					'timeout' => 100
+//				)
+//			)
+//		);
+//
+//		$contents = @file_get_contents($url . "/job/" . $job . "/lastSuccessfulBuild/", 0, $ctx);
+//		$matches = array();
+//		$match = preg_match('/\<title\>[A-Za-z0-9-_]+ \#([0-9]+) \[Jenkins\]\<\/title\>/i', $contents, $matches);
+//		if ($match == 0){
+//			$array = array(
+//				'exists' => false,
+//				'build' => 'None',
+//				'url' => '#'
+//			);
+//		}else{
+//			$buildnum = $matches[1];
+//			$array = array(
+//				'exists' => true,
+//				'build' => $buildnum,
+//				'url' => $url.'/job/'.$job.'/'.$buildnum
+//			);
+//		}
+//		return $array;
 	}
 
 }

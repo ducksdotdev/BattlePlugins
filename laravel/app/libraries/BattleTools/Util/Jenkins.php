27be91c729a88efe80ad77feea55335133f3c7d6
Justin <justin@battleplugins.com>
2014-05-07 15:56:25 -0400

    Update Jenkins class

--- laravel/app/libraries/BattleTools/Util/Jenkins.php
+++ laravel/app/libraries/BattleTools/Util/Jenkins.php
@@ -5,31 +5,30 @@ namespace BattleTools\Util;
 class Jenkins {
 
 	public static function getLatestBuild($url, $job){
-		$request = Requests::get($url . "/job/" . $job . "/lastSuccessfulBuild/", array(), array(
-			'timeout' => '3'
-		));
+		$dne = array(
+			'exists' => false,
+			'build' => 'None',
+			'url' => '#'
+		);
+
+		$request = Requests::get($url . "/job/" . $job . "/lastSuccessfulBuild/", array(), array('timeout' => '3'));
 
 		if($request->success) {
 			$matches = array();
-			$match = preg_match('/\<title\>[A-Za-z0-9-_]+ \#([0-9]+) \[Jenkins\]\<\/title\>/i', $contents, $matches);
+			$match = preg_match('/\<title\>[A-Za-z0-9-_]+ \#([0-9]+) \[Jenkins\]\<\/title\>/i', $request->body, $matches);
 			if ($match == 0) {
-				$array = array(
-					'exists' => false,
-					'build' => 'None',
-					'url' => '#'
-				);
+				return $dne;
 			} else {
 				$buildnum = $matches[1];
-				$array = array(
+				return array(
 					'exists' => true,
 					'build' => $buildnum,
 					'url' => $url . '/job/' . $job . '/' . $buildnum
 				);
 			}
-			return $array;
 		}else{
 			Log::emergency("It looks like the CI server is down at $url");
-			return array('exists'=>false);
+			return $dne;
 		}
 	}
 

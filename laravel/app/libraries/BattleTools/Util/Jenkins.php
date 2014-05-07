f14ad92aa35aa0cad5b87727f6c5284f41baf6e7
Justin <justin@battleplugins.com>
2014-05-07 16:23:13 -0400

    Cache the timeout

--- laravel/app/libraries/BattleTools/Util/Jenkins.php
+++ laravel/app/libraries/BattleTools/Util/Jenkins.php
@@ -2,29 +2,35 @@
 
 namespace BattleTools\Util;
 
+use Carbon\Carbon;
+use Illuminate\Support\Facades\Cache;
 use Illuminate\Support\Facades\Log;
 
 class Jenkins {
 
 	public static function getLatestBuild($url, $job){
-		try {
-			$request = \Requests::get($url . "/job/" . $job . "/lastSuccessfulBuild/", array(), array('timeout' => 2));
-		}catch(\Requests_Exception $e){
-			return self::timeout($url);
-		}
-
-		if($request->success) {
-			$matches = array();
-			$match = preg_match('/\<title\>[A-Za-z0-9-_]+ \#([0-9]+) \[Jenkins\]\<\/title\>/i', $request->body, $matches);
-			if ($match == 0) {
+		if(!Cache::get('timeoutBuffer')) {
+			try {
+				$request = \Requests::get($url . "/job/" . $job . "/lastSuccessfulBuild/", array(), array('timeout' => 2));
+			}catch (\Requests_Exception $e) {
 				return self::timeout($url);
+			}
+
+			if ($request->success) {
+				$matches = array();
+				$match = preg_match('/\<title\>[A-Za-z0-9-_]+ \#([0-9]+) \[Jenkins\]\<\/title\>/i', $request->body, $matches);
+				if ($match == 0) {
+					return self::timeout($url);
+				} else {
+					$buildnum = $matches[1];
+					return array(
+						'exists' => true,
+						'build' => $buildnum,
+						'url' => $url . '/job/' . $job . '/' . $buildnum
+					);
+				}
 			} else {
-				$buildnum = $matches[1];
-				return array(
-					'exists' => true,
-					'build' => $buildnum,
-					'url' => $url . '/job/' . $job . '/' . $buildnum
-				);
+				return self::timeout($url);
 			}
 		}else{
 			return self::timeout($url);
@@ -32,6 +38,7 @@ class Jenkins {
 	}
 
 	private static function timeout($url){
+		Cache::put('timeoutBuffer', true, Carbon::now()->addHours(12));
 		Log::emergency("It looks like the CI server is down at $url.");
 		return array(
 			'exists' => false,

3c5551bcf5ae7a13c60f4bd20d442b44e3922bb7
Justin <justin@battleplugins.com>
2015-06-01 20:24:02 -0400

    Add content

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -32,127 +32,105 @@
         </div>
     </div>
 </nav>
+{{--<div class="grid-container">--}}
+    {{--<div class="ui items">--}}
+        {{--<div class="grid-25">--}}
+            {{--<div class="ui segment">--}}
+                {{--<div class="item">--}}
+                    {{--<div class="content">--}}
+                        {{--<span class="header">BattleArena v1.2.3 <small class="version-update">Updated 2 hours ago.</small></span>--}}
+                        {{--<div class="description">--}}
+                            {{--<p>BattleArena has just been updated to version 1.2.3. Download the latest version from <a href="#">Jenkins</a>.</p>--}}
+                        {{--</div>--}}
+                    {{--</div>--}}
+                {{--</div>--}}
+            {{--</div>--}}
+        {{--</div>--}}
+        {{--<div class="grid-25">--}}
+            {{--<div class="ui segment">--}}
+                {{--<div class="item">--}}
+                    {{--<div class="content">--}}
+                        {{--<span class="header">BattleArena v1.2.3</span>--}}
+                        {{--<div class="description">--}}
+                            {{--<p>BattleArena has just been updated to version 1.2.3. Download the latest version from <a href="#">Jenkins</a>.</p>--}}
+                        {{--</div>--}}
+                    {{--</div>--}}
+                {{--</div>--}}
+            {{--</div>--}}
+        {{--</div>--}}
+        {{--<div class="grid-25">--}}
+            {{--<div class="ui segment">--}}
+                {{--<div class="item">--}}
+                    {{--<div class="content">--}}
+                        {{--<span class="header">BattleArena v1.2.3</span>--}}
+                        {{--<div class="description">--}}
+                            {{--<p>BattleArena has just been updated to version 1.2.3. Download the latest version from <a href="#">Jenkins</a>.</p>--}}
+                        {{--</div>--}}
+                    {{--</div>--}}
+                {{--</div>--}}
+            {{--</div>--}}
+        {{--</div>--}}
+        {{--<div class="grid-25">--}}
+            {{--<div class="ui segment">--}}
+                {{--<div class="item">--}}
+                    {{--<div class="content">--}}
+                        {{--<span class="header">BattleArena v1.2.3</span>--}}
+                        {{--<div class="description">--}}
+                            {{--<p>BattleArena has just been updated to version 1.2.3. Download the latest version from <a href="#">Jenkins</a>.</p>--}}
+                        {{--</div>--}}
+                    {{--</div>--}}
+                {{--</div>--}}
+            {{--</div>--}}
+        {{--</div>--}}
+    {{--</div>--}}
+{{--</div>--}}
 <div class="grid-container">
-    <div class="ui items">
-        <div class="grid-25">
-            <div class="ui segment">
-                <div class="item">
-                    <div class="content">
-                        <span class="header">BattleArena v1.2.3 <small class="version-update">Updated 2 hours ago.</small></span>
-                        <div class="description">
-                            <p>BattleArena has just been updated to version 1.2.3. Download the latest version from <a href="#">Jenkins</a>.</p>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div>
-        <div class="grid-25">
-            <div class="ui segment">
-                <div class="item">
-                    <div class="content">
-                        <span class="header">BattleArena v1.2.3</span>
-                        <div class="description">
-                            <p>BattleArena has just been updated to version 1.2.3. Download the latest version from <a href="#">Jenkins</a>.</p>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div>
-        <div class="grid-25">
-            <div class="ui segment">
-                <div class="item">
-                    <div class="content">
-                        <span class="header">BattleArena v1.2.3</span>
-                        <div class="description">
-                            <p>BattleArena has just been updated to version 1.2.3. Download the latest version from <a href="#">Jenkins</a>.</p>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div>
-        <div class="grid-25">
-            <div class="ui segment">
-                <div class="item">
-                    <div class="content">
-                        <span class="header">BattleArena v1.2.3</span>
-                        <div class="description">
-                            <p>BattleArena has just been updated to version 1.2.3. Download the latest version from <a href="#">Jenkins</a>.</p>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div>
-    </div>
-</div>
-<div class="grid-container">
-    <div class="grid-75" id="blog">
+    <div class="grid-100" id="blog">
         <div class="item">
             <div class="content">
                 <h1>
-                    Proident non Quis <small class="author">Written by lDucks 20 minutes ago.</small>
+                    New Blog! <small class="author">Written by Zach443.</small>
                 </h1>
                 <div class="description">
-                    <p>Non aliqua probant litteris, vidisse se fugiat singulis e quae iis nostrud o
-                        multos, id dolor multos a excepteur. Ea ne graviterque, malis officia est
-                        nescius, singulis enim culpa ubi aute sed eu sint tempor distinguantur. Labore
-                        occaecat ne relinqueret quo esse mandaremus exercitation ex sed dolor proident
-                        concursionibus iis incurreret nisi aut ingeniis eruditionem. Amet arbitrantur
-                        nescius irure iudicem in magna iudicem hic sint quorum. Sunt adipisicing
-                        quamquam nisi litteris quo iis quae irure eu laboris o occaecat a summis
-                        vidisse, sed hic cohaerescant, deserunt illum ita cupidatat tractavissent,
-                        aliquip dolore mentitum possumus an fabulas quae si doctrina distinguantur, enim
-                        ex sed quis senserit. Quae ab ingeniis te do noster quis in doctrina qui malis
-                        deserunt aut reprehenderit, irure commodo et exercitation, ullamco quid enim
-                        proident lorem ubi sed elit appellat graviterque an incurreret ita duis
-                        mandaremus sed irure a singulis ita multos. Aut o consectetur.
-                    </p>
                     <p>
-                        Nostrud aliqua ut litteris relinqueret in offendit cillum laboris quamquam. Eu
-                        quae est quorum te est elit sunt magna ullamco est ne te irure cupidatat o
-                        nescius dolor vidisse admodum id sed incurreret se quamquam, te ubi quem
-                        vidisse, de fore iudicem a incididunt do ullamco. Occaecat ne eram incurreret.
-                        Minim arbitrantur cupidatat multos commodo. Ab anim nescius excepteur ad non
-                        pariatur do cernantur, do cillum iudicem familiaritatem in esse voluptate
-                        domesticarum, magna a cupidatat, ab tamen e summis an expetendis esse an commodo
-                        exquisitaque sed est dolore probant sempiternum. Commodo elit laboris, e anim
-                        quamquam exquisitaque, aut quo illustriora quo illum vidisse quo summis magna.
+                        Hey guys, this is our new blog/website! I will be posting things like development tutorials, progress/dev updates on all of the BattlePlugins, as well as any other information about BattlePlugins here. All kinds of exciting things will be coming out of this blog. Aside from the blog, this site will contain links to all BattlePlugins' resources, such as the wiki, GitHub, and Jenkins. Above this post there will be boxes that will show all the most recent updates to our plugins. Enjoy the new site!
                     </p>
                 </div>
             </div>
         </div>
     </div>
-    <div class="grid-25">
-        <h4>Latest Blog Posts:</h4>
-        <div id="latestBlogPosts" class="ui divided list">
-            <div class="item">
-                <div class="content">
-                    <a class="header">Quae te</a>
-                    <div class="description">Exercitation fabulas varias id iis aliqua fore tractavissent ita amet
-                        sempiternum relinqueret. Officia id laboris commodo...</div>
-                </div>
-            </div>
-            <div class="item">
-                <div class="content">
-                    <a class="header">Labore Transferrem Elit</a>
-                    <div class="description">Deserunt nescius comprehenderit duis quorum multos quamquam o vidisse appellat
-                        cernantur singulis...</div>
-                </div>
-            </div>
-            <div class="item">
-                <div class="content">
-                    <a class="header">Comprehenderit Fabulas</a>
-                    <div class="description">Varias distinguantur aut possumus voluptate litteris concursionibus culpa legam
-                        fore...</div>
-                </div>
-            </div>
-            <div class="item">
-                <div class="content">
-                    <a class="header">Appellat Tamen aut nam Dolore Expetendis aut</a>
-                    <div class="description">Voluptate quid quamquam et appellat nam magna nescius. Id fore e multos, ea enim tamen qui incididunt...</div>
-                </div>
-            </div>
-        </div>
-    </div>
+    {{--<div class="grid-25">--}}
+        {{--<h4>Latest Blog Posts:</h4>--}}
+        {{--<div id="latestBlogPosts" class="ui divided list">--}}
+            {{--<div class="item">--}}
+                {{--<div class="content">--}}
+                    {{--<a class="header">Quae te</a>--}}
+                    {{--<div class="description">Exercitation fabulas varias id iis aliqua fore tractavissent ita amet--}}
+                        {{--sempiternum relinqueret. Officia id laboris commodo...</div>--}}
+                {{--</div>--}}
+            {{--</div>--}}
+            {{--<div class="item">--}}
+                {{--<div class="content">--}}
+                    {{--<a class="header">Labore Transferrem Elit</a>--}}
+                    {{--<div class="description">Deserunt nescius comprehenderit duis quorum multos quamquam o vidisse appellat--}}
+                        {{--cernantur singulis...</div>--}}
+                {{--</div>--}}
+            {{--</div>--}}
+            {{--<div class="item">--}}
+                {{--<div class="content">--}}
+                    {{--<a class="header">Comprehenderit Fabulas</a>--}}
+                    {{--<div class="description">Varias distinguantur aut possumus voluptate litteris concursionibus culpa legam--}}
+                        {{--fore...</div>--}}
+                {{--</div>--}}
+            {{--</div>--}}
+            {{--<div class="item">--}}
+                {{--<div class="content">--}}
+                    {{--<a class="header">Appellat Tamen aut nam Dolore Expetendis aut</a>--}}
+                    {{--<div class="description">Voluptate quid quamquam et appellat nam magna nescius. Id fore e multos, ea enim tamen qui incididunt...</div>--}}
+                {{--</div>--}}
+            {{--</div>--}}
+        {{--</div>--}}
+    {{--</div>--}}
 </div>
 <footer>
     <p>

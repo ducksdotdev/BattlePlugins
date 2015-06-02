08356d945a0b6a4b87efb7f15d7854b088ea0440
Justin <justin@battleplugins.com>
2015-06-01 22:09:24 -0400

    Add logout button

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -135,7 +135,11 @@
 @endif
 <footer>
     <p>
-        <a href="http://ci.battleplugins.com">Jenkins</a> | <a href="http://wiki.battleplugins.com">Wiki</a> | <a href="http://github.com/BattlePlugins">GitHub</a> | <a href="http://tasks.battleplugins.com">BattleTasks</a> | <a href="http://api.battleplugins.com">BattleWebAPI</a><br/>
+        <a href="http://ci.battleplugins.com">Jenkins</a> | <a href="http://wiki.battleplugins.com">Wiki</a> | <a href="http://github.com/BattlePlugins">GitHub</a> | <a href="http://tasks.battleplugins.com">BattleTasks</a> | <a href="http://api.battleplugins.com">BattleWebAPI</a>
+        @if(Auth::check())
+            | <a href="/logout">Logout</a>
+        @endif
+        <br/>
         Copyright &copy; BattlePlugins. All Rights Reserved.<br/>
         BattlePlugins is not affiliated with Mojang or Minecraft.
     </p>

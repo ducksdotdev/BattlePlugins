acb6d54f9b08a2a063b1ad09f7c8d0b5031e6c18
lDucks <justin@battleplugins.com>
2015-06-16 13:49:13 -0400

    Change blog check

--- app/Http/Controllers/Blog/PageController.php
+++ app/Http/Controllers/Blog/PageController.php
@@ -6,35 +6,36 @@ use App\Tools\Models\User;
 
 class PageController extends Controller {
 
-	/**
-	 * Show the application welcome screen to the user.
-	 *
-	 * @return Response
-	 */
-	public function index()
-	{
-		$blog = Blog::latest()->first()->id;
-		return $this->getBlog($blog);
-	}
+    /**
+     * Show the application welcome screen to the user.
+     *
+     * @return Response
+     */
+    public function index()
+    {
+        $blog = Blog::latest()->first()->id;
+        return $this->getBlog($blog);
+    }
 
     public function getBlog($id)
     {
         $blog = Blog::find($id);
-		$users = User::all();
-		$displaynames = [];
 
-		foreach ($users as $user)
-			$displaynames[$user->id] = $user->displayname;
-
-        if ($blog) {
-            return view('blog.index', [
-                'blog' => $blog,
-                'list' => Blog::latest()->take(4)->get(),
-                'author' => $displaynames[$blog->author],
-                'users' => $displaynames
-            ]);
-        } else
+        if (!$blog)
             return view('blog.index');
-	}
+
+        $users = User::all();
+        $displaynames = [];
+
+        foreach ($users as $user)
+            $displaynames[$user->id] = $user->displayname;
+
+        return view('blog.index', [
+            'blog' => $blog,
+            'list' => Blog::latest()->take(4)->get(),
+            'author' => $displaynames[$blog->author],
+            'users' => $displaynames
+        ]);
+    }
 
 }

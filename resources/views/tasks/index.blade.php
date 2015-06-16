b154b4ef6920bf71894527222af3340d3e7651f6
lDucks <justin@battleplugins.com>
2015-06-15 20:22:59 -0400

    Fix styles

--- resources/views/tasks/index.blade.php
+++ resources/views/tasks/index.blade.php
@@ -57,51 +57,7 @@
             @else
                 <div class="ui divided list">
                     @foreach($tasks as $task)
-                        <div class="item
-					@if($task->status)
-						completed
-					@endif" id="task{{ $task->id }}"
-                             ng-class="{highlighted: {{ $task->id }} == highlighted}"
-                             ng-hide="{{ $task->status }} && !showCompleted">
-                            <div class="content @if(Auth::check()) grid-90 @else grid-100 @endif">
-                                <div class="header">
-                                    <a href="#task{{ $task->id }}" ng-click="setHighlighted({{ $task->id }})"><i
-                                                class="icon linkify"></i></a>
-                                    {{$task->title}}
-                                    @if(Auth::check() && $task->public)
-                                        (Public)
-                                    @endif
-                                    <small>Assigned
-                                        @if($task->assigned_to != 0)
-                                            to
-                                            <span class="name">
-											{{ User::find($task->assigned_to)->displayname }}
-										</span>
-                                        @endif
-                                        by
-									<span class="name">
-										{{ User::find($task->creator)->displayname }}
-									</span>
-                                    </small>
-                                </div>
-                                <div class="description editable">
-                                    {!! strip_tags(Linkify::link($task->content), '<a><br>') !!}
-                                </div>
-                            </div>
-                            @if(Auth::check())
-                                <div class="actions grid-10 text-right">
-                                    <div ng-click="completeTask($event, {{ $task->id }})"
-                                         class="pull-left circular small ui positive icon button"
-                                         ng-class="{disabled: {{ $task->status }} || {{$task->creator}} == 24}">
-                                        <i class="icon check"></i>
-                                    </div>
-                                    <div ng-click="deleteTask($event, {{ $task->id }})"
-                                         class="delete-task pull-left circular red small ui icon button">
-                                        <i class="icon trash"></i>
-                                    </div>
-                                </div>
-                            @endif
-                        </div>
+                        @include('tasks.partials.task')
                     @endforeach
                 </div>
             @endif
--- resources/views/tasks/partials/styles.blade.php
+++ resources/views/tasks/partials/styles.blade.php
@@ -1,6 +1,6 @@
 <!--        Styles -->
-<link rel="stylesheet" href="/assets/css/styles.css" type="text/css"/>
-<link rel="stylesheet" href="/assets/css/semantic.css">
+<link rel="stylesheet" href="/assets/css/styles.css"/>
+<link rel="stylesheet" href="/assets/css/semantic.css"/>
 <link rel="stylesheet" href="//cdnjs.cloudflare.com/ajax/libs/semantic-ui/1.12.0/components/icon.css"/>
-<link rel="stylesheet" href="//cdnjs.cloudflare.com/ajax/libs/unsemantic/0/unsemantic-grid-responsive.css">
+<link rel="stylesheet" href="//cdnjs.cloudflare.com/ajax/libs/unsemantic/0/unsemantic-grid-responsive.css"/>
 <!--        End Styles -->

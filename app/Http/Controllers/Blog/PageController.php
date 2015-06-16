9f81f6a4398b0094c1a4eddf6db36e53b02cb977
lDucks <justin@battleplugins.com>
2015-06-16 14:47:06 -0400

    Create retrieve method

--- app/Http/Controllers/Blog/PageController.php
+++ app/Http/Controllers/Blog/PageController.php
@@ -13,8 +13,29 @@ class PageController extends Controller {
      */
     public function index()
     {
-        $blog = Blog::latest()->first()->id;
-        return $this->getBlog($blog);
+        $blog = Blog::latest()->first();
+
+        if (!$blog)
+            return view('blog.index');
+
+        return view('blog.index', $this->retrieve($blog));
+    }
+
+    private function retrieve(Blog $blog)
+    {
+
+        $users = User::all();
+        $displaynames = [];
+
+        foreach ($users as $user)
+            $displaynames[$user->id] = $user->displayname;
+
+        return [
+            'blog' => $blog,
+            'list' => Blog::latest()->take(4)->get(),
+            'author' => $displaynames[$blog->author],
+            'users' => $displaynames
+        ];
     }
 
     public function getBlog($id)
@@ -24,18 +45,7 @@ class PageController extends Controller {
         if (!$blog)
             return view('blog.index');
 
-        $users = User::all();
-        $displaynames = [];
-
-        foreach ($users as $user)
-            $displaynames[$user->id] = $user->displayname;
-
-        return view('blog.index', [
-            'blog' => $blog,
-            'list' => Blog::latest()->take(4)->get(),
-            'author' => $displaynames[$blog->author],
-            'users' => $displaynames
-        ]);
+        return view('blog.index', $this->retrieve($blog));
     }
 
 }

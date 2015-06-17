e700d5841311718507538f463e4ed704c322bbb3
lDucks <justin@battleplugins.com>
2015-06-16 20:58:15 -0400

    Status is int

--- app/Http/Controllers/Tasks/TasksController.php
+++ app/Http/Controllers/Tasks/TasksController.php
@@ -78,6 +78,6 @@ class TasksController extends Controller
 
     public function completeTask($id)
     {
-        Task::findOrFail($id)->update('status', '1');
+        Task::findOrFail($id)->update('status', 1);
     }
 }

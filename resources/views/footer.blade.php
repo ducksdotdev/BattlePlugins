58c9e96b4d4b625b6b075be3033d42a485687c2e
lDucks <justin@battleplugins.com>
2015-06-19 12:42:50 -0400

    Change the footer paragraphs a bit

--- resources/views/footer.blade.php
+++ resources/views/footer.blade.php
@@ -3,14 +3,16 @@
         <a href="http://ci.battleplugins.com">Jenkins</a> | <a href="http://wiki.battleplugins.com">Wiki</a> | <a
                 href="http://github.com/BattlePlugins">GitHub</a> | <a
                 href="http://tasks.battleplugins.com">BattleTasks</a> | <a href="http://api.battleplugins.com">BattleWebAPI</a>
-        |
-        <a href="http://bplug.in">bplug.in</a>
-        @if(Auth::check())
+        | <a href="http://bplug.in">bplug.in</a>
+    </p>
+    @if(Auth::check())
+        <p>
             <a href="https://admin.battleplugins.com/">User Settings</a>
             | <a href="/logs">Log Viewer</a>
             | <a href="/logout">Logout</a>
-        @endif
-        <br/>
+        </p>
+    @endif
+    <p>
         Copyright &copy; BattlePlugins. All Rights Reserved.<br/>
         BattlePlugins is not affiliated with Mojang or Minecraft.
     </p>

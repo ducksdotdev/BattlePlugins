95b82d09ca13e7d9de8f9a158394b3b2261c236f
lDucks <justin@battleplugins.com>
2015-06-19 12:36:08 -0400

    500 error

---
+++ resources/views/errors/500.blade.php
@@ -0,0 +1,61 @@
+<html>
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="author" content="lDucks">
+    <title>BattlePlugins :: Returning Soon</title>
+    <link rel="icon" type="image/png" href="favicon.png"/>
+    <style>
+        body {
+            text-align: center;
+            padding: 150px;
+        }
+
+        h1 {
+            font-size: 50px;
+        }
+
+        body {
+            font: 20px Helvetica, sans-serif;
+            color: #333;
+        }
+
+        article {
+            display: block;
+            text-align: left;
+            width: 650px;
+            margin: 0 auto;
+        }
+
+        a {
+            color: #428bca;
+            text-decoration: none;
+        }
+
+        a:hover {
+            color: #2a6496;
+            text-decoration: underline;
+        }
+
+        footer {
+            margin: 20px 0;
+            color: #a3a3a3;
+            text-align: center;
+            font-size: 12px;
+        }
+    </style>
+</head>
+<body>
+<h1>Oh No! Looks like something went wrong!</h1>
+
+<div>
+    <p>
+    <h4>We couldn't load the page you were looking for! (500 Error)</h4>
+
+    <h3>Please report this error in the <a href="http://irc.battleplugins.com/">#battleplugins IRC channel</a>.</h3>
+    <h4><a href="/">Click here to Return to Safety</a></h4>
+    </p>
+    @include('footer')
+</div>
+</body>
+</html>

2616fc4bcb12597a6aa32c60bb261a195f861917
lDucks <justin@battleplugins.com>
2015-06-21 00:51:23 -0400

    405 Error

---
+++ resources/views/errors/405.blade.php
@@ -0,0 +1,31 @@
+<html>
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="author" content="lDucks">
+    <title>BattlePlugins :: 405 Error</title>
+    <link rel="icon" type="image/png" href="bp.png"/>
+    <link rel="stylesheet" href="//cdnjs.cloudflare.com/ajax/libs/unsemantic/0/unsemantic-grid-responsive.css">
+    <link rel="stylesheet" href="/assets/css/error.css"/>
+</head>
+<body>
+<div class="grid-container">
+    <div class="grid-100">
+        <h1>
+            405 Method Not Allowed<br/>
+            Oh No! Looks like something went wrong!
+        </h1>
+    </div>
+    <div class="grid-100">
+        <h4>
+            We couldn't access the page you were looking for!<br/>
+            Please report this error in the <a href="http://irc.battleplugins.com/">#battleplugins IRC channel</a>.
+        </h4>
+        <h4><a href="/">Click here to Return to Safety</a></h4>
+    </div>
+    <div class="grid-100">
+        @include('footer')
+    </div>
+</div>
+</body>
+</html>

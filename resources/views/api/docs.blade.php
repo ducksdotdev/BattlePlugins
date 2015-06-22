bb7c1908b5ed28717d7e51e2532bed423d3162ed
lDucks <justin@battleplugins.com>
2015-06-22 13:12:57 -0400

    Change plugin api link

--- resources/views/api/docs.blade.php
+++ resources/views/api/docs.blade.php
@@ -5,7 +5,10 @@
             <div class="grid-100">
                 <h2>API Documentation (BattleWebAPI {{env('API_VERSION')}}) <button id="manageWebhooks" class="ui small button default">Manage Webhooks</button></h2>
                 <p>Your API key is <strong>{{ $apiKey }}</strong>. Please do not share this key with anyone. It allows other users or servers to act on your behalf. If your key is compromised, you can <a href="/generateKey">generate a new one</a>.</p>
-                <p><a href="https://github.com/alkarinv/BattlePluginsAPI/blob/master/src/java/mc/battleplugins/api/BattlePluginsAPI.java" target="_blank">You can also use our BattlePluginsAPI in your plugins in order to make post requests, pastes, and use our statistics for your plugins.</a></p>
+
+                <p><a href="https://github.com/BattlePlugins/BattleWebApi.java" target="_blank">You can also use our
+                        BattleWebAPI.java in your plugins in order to make post requests, pastes, and use our statistics
+                        for your plugins.</a></p>
             </div>
             <div class="grid-100" id="authentication">
                 <h3>Authentication</h3>

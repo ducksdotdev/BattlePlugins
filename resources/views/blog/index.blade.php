1da74ea80a1aeb0e30ffe1be28614a2a5018f17b
lDucks <justin@battleplugins.com>
2015-06-21 20:32:13 -0400

    Split jenkins items into views

--- resources/views/blog/index.blade.php
+++ resources/views/blog/index.blade.php
@@ -45,28 +45,7 @@
         </div>
     </div>
 </nav>
-<div class="grid-container">
-    <div class="ui items">
-        @foreach($rssFeed as $item)
-            <div class="grid-25">
-                <div class="ui segment">
-                    <div class="item">
-                        <div class="content">
-                            <span class="header">{{ $item->get_title() }}</span>
-
-                            <div class="description">
-                                <p>Last updated <span
-                                            title="{{ $item->get_date() }}">{{ (new \Carbon\Carbon($item->get_date()))->diffForHumans() }}</span>.
-                                    Download the latest version from <a href="{{ $item->get_permalink()
-                                }}">Jenkins</a>.</p>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        @endforeach
-    </div>
-</div>
+{{--@include('blog.partials.jenkins')--}}
 @if(isset($blog))
     <div class="grid-container">
         <div class="grid-75 grid-parent" id="blog">
---
+++ resources/views/blog/partials/jenkins.blade.php
@@ -0,0 +1,5 @@
+<div class="grid-container">
+    <div class="ui items">
+        @include('blog.partials.jenkinsitem')
+    </div>
+</div>

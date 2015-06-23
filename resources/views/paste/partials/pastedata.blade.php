29da143994eec6d8f1aedb0aae32c1717153772a
Justin <justin@battleplugins.com>
2015-06-23 16:08:27 -0400

    Move links to end of input

--- resources/views/paste/partials/pastedata.blade.php
+++ resources/views/paste/partials/pastedata.blade.php
@@ -2,24 +2,40 @@
     <div class="three fields">
         <div class="field">
             <label>
-                <a href="http://bplug.in/{{ $paste->slug }}" class="circular small ui icon button"><i class="icon external"></i></a>
                 Short URL:
             </label>
-            <input type="text" onclick="select()" value="http://bplug.in/{{ $paste->slug }}" readonly />
+            <div class="ui action input">
+                <input type="text" onclick="select()" value="http://bplug.in/{{ $paste->slug }}" readonly />
+                <a class="ui right labeled icon button" href="http://bplug.in/{{ $paste->slug }}">
+                    <i class="external icon"></i>
+                    Go
+                </a>
+            </div>
         </div>
         <div class="field">
             <label>
-                <a href="/{{ $paste->slug }}/raw" class="circular small ui icon button"><i class="icon external"></i></a>
                 Raw URL:
             </label>
-            <input type="text" onclick="select()" value="http://paste.battleplugins.com/{{ $paste->slug }}/raw" readonly />
+            <div class="ui action input">
+                <input type="text" onclick="select()" value="http://paste.battleplugins.com/{{ $paste->slug }}/raw" readonly />
+                <a class="ui right labeled icon button" href="/{{ $paste->slug }}/raw">
+                    <i class="external icon"></i>
+                    Go
+                </a>
+            </div>
         </div>
         <div class="field">
             <label>
                 <a href="/{{ $paste->slug }}/download" class="circular small ui icon button"><i class="icon external"></i></a>
                 Download Link:
             </label>
-            <input type="text" onclick="select()" value="http://paste.battleplugins.com/{{ $paste->slug }}/download" readonly />
+            <div class="ui action input">
+                <input type="text" onclick="select()" value="http://paste.battleplugins.com/{{ $paste->slug }}/download" readonly />
+                <a class="ui right labeled icon button" href="/{{ $paste->slug }}/download">
+                    <i class="external icon"></i>
+                    Go
+                </a>
+            </div>
         </div>
     </div>
     <div class="text-center">

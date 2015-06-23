819849023a4ac3e8298ae8c06f12beba03a74d71
Justin <justin@battleplugins.com>
2015-06-23 15:49:22 -0400

    Use read only

--- resources/views/paste/partials/pastedata.blade.php
+++ resources/views/paste/partials/pastedata.blade.php
@@ -2,23 +2,26 @@ Created <span title="{{ $paste->created_at }}">{{ $paste->created_at->diffForHum
 @if($paste->created_at != $paste->updated_at)
     Last modified <span title="{{ $paste->updated_at }}">{{ $paste->updated_at->diffForHumans() }}</span>.
 @endif<br />
-<div class="field">
-    <label>
-        Short URL:
-        <a href="http://bplug.in/{{ $paste->slug }}" class="circular small ui icon button"><i class="icon external"></i></a>
-    </label>
-    <input type="text" onclick="select()" value="http://bplug.in/{{ $paste->slug }}" disabled />
-</div>
-<div class="field">
-    <label>
-        Raw URL:
-        <a href="/{{ $paste->slug }}/raw" class="circular small ui icon button"><i class="icon external"></i></a>
-    </label>
-    <input type="text" onclick="select()" value="http://paste.battleplugins.com/{{ $paste->slug }}/raw" disabled />
-</div>
-<div class="field">
-    <label>Download Link:
-        <a href="/{{ $paste->slug }}/download" class="circular small ui icon button"><i class="icon external"></i></a>
-    </label>
-    <input type="text" onclick="select()" value="http://paste.battleplugins.com/{{ $paste->slug }}/download" disabled />
+<div class="fields">
+    <div class="field">
+        <label>
+            <a href="http://bplug.in/{{ $paste->slug }}" class="circular small ui icon button"><i class="icon external"></i></a>
+            Short URL:
+        </label>
+        <input type="text" onclick="select()" value="http://bplug.in/{{ $paste->slug }}" readonly />
+    </div>
+    <div class="field">
+        <label>
+            <a href="/{{ $paste->slug }}/raw" class="circular small ui icon button"><i class="icon external"></i></a>
+            Raw URL:
+        </label>
+        <input type="text" onclick="select()" value="http://paste.battleplugins.com/{{ $paste->slug }}/raw" readonly />
+    </div>
+    <div class="field">
+        <label>
+            <a href="/{{ $paste->slug }}/download" class="circular small ui icon button"><i class="icon external"></i></a>
+            Download Link:
+        </label>
+        <input type="text" onclick="select()" value="http://paste.battleplugins.com/{{ $paste->slug }}/download" readonly />
+    </div>
 </div>

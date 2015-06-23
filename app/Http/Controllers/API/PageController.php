e1a9282d0b2542d6c352639eaa43fd5c3ce91501
Justin <justin@battleplugins.com>
2015-06-23 13:08:31 -0400

    Move authentication checks

--- app/Http/Controllers/API/PageController.php
+++ app/Http/Controllers/API/PageController.php
@@ -11,34 +11,34 @@ use Auth;
 
 class PageController extends Controller {
 
-	function __construct(){
-		if(!Auth::check())
+	public function index(){
+		if(Auth::check()) {
+			$apiKey = Auth::user()->api_key;
+			if (!$apiKey) {
+				$apiKey = GenerateApiKey::generateKey();
+				User::find(Auth::user()->id)->update([
+					'api_key' => $apiKey
+				]);
+			}
+
+			$webhooks = Webhook::whereUser(Auth::user()->id)->get();
+
+			return view('api.docs', [
+				'apiKey' => $apiKey,
+				'docs' => Config::get('api.docs'),
+				'webhooks' => Webhooks::getTypes(),
+				'myHooks' => $webhooks
+			]);
+		} else
 			return view('api.login');
 	}
 
-	public function index(){
-		$apiKey = Auth::user()->api_key;
-		if (!$apiKey) {
-			$apiKey = GenerateApiKey::generateKey();
-			User::find(Auth::user()->id)->update([
-				'api_key' => $apiKey
-			]);
-		}
-
-		$webhooks = Webhook::whereUser(Auth::user()->id)->get();
-
-		return view('api.docs', [
-			'apiKey' => $apiKey,
-			'docs' => Config::get('api.docs'),
-			'webhooks' => Webhooks::getTypes(),
-			'myHooks' => $webhooks
-		]);
-	}
-
 	public function generateKey(){
-		$user = Auth::user();
-		$user->api_key = GenerateApiKey::generateKey();
-		$user->save();
+		if(Auth::check()) {
+			$user = Auth::user();
+			$user->api_key = GenerateApiKey::generateKey();
+			$user->save();
+		}
 
 		return redirect('/');
 	}

57bccfc0d82f37c690371780ba5277a9e7809474
Justin <justin@battleplugins.com>
2015-06-23 16:11:10 -0400

    Remove the go label

--- resources/views/paste/partials/pastedata.blade.php
+++ resources/views/paste/partials/pastedata.blade.php
@@ -6,9 +6,8 @@
             </label>
             <div class="ui action input">
                 <input type="text" onclick="select()" value="http://bplug.in/{{ $paste->slug }}" readonly />
-                <a class="ui right labeled icon button" href="http://bplug.in/{{ $paste->slug }}">
+                <a class="ui right icon button" href="http://bplug.in/{{ $paste->slug }}">
                     <i class="external icon"></i>
-                    Go
                 </a>
             </div>
         </div>
@@ -18,22 +17,19 @@
             </label>
             <div class="ui action input">
                 <input type="text" onclick="select()" value="http://paste.battleplugins.com/{{ $paste->slug }}/raw" readonly />
-                <a class="ui right labeled icon button" href="/{{ $paste->slug }}/raw">
+                <a class="ui right icon button" href="/{{ $paste->slug }}/raw">
                     <i class="external icon"></i>
-                    Go
                 </a>
             </div>
         </div>
         <div class="field">
             <label>
-                <a href="/{{ $paste->slug }}/download" class="circular small ui icon button"><i class="icon external"></i></a>
                 Download Link:
             </label>
             <div class="ui action input">
                 <input type="text" onclick="select()" value="http://paste.battleplugins.com/{{ $paste->slug }}/download" readonly />
-                <a class="ui right labeled icon button" href="/{{ $paste->slug }}/download">
+                <a class="ui right icon button" href="/{{ $paste->slug }}/download">
                     <i class="external icon"></i>
-                    Go
                 </a>
             </div>
         </div>

16b582fb6553f3a069334bb335de5b2107b1d116
lDucks <justin@battleplugins.com>
2015-06-24 17:48:59 -0400

    Update sass-compile script

--- sass-compile.sh
+++ sass-compile.sh
@@ -1,2 +1,2 @@
 #!/usr/bin/env bash
-sass resources/assets/sass/master.scss:public/assets/css/styles.css && sass resources/assets/sass/errors.scss:public/assets/css/errors.css
+sass /resources/assets/sass/master.scss:/public/assets/css/styles.css && sass /resources/assets/sass/errors.scss:/public/assets/css/errors.css

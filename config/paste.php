7b2ac21c612e0680d2bd6d653f80c8667517d336
lDucks <justin@battleplugins.com>
2015-06-24 17:45:45 -0400

    Test sass-compile

--- config/paste.php
+++ config/paste.php
@@ -1,5 +1,3 @@
 <?php
 
-return ["BSH", "C", "CC", "CPP", "CS", "CSS", "CSH", "CYC", "CV", "HTM", "HTML",
-    "JAVA", "JS", "M", "MXML", "PERL", "PL", "PM", "PY", "RB", "SH",
-    "XHTML", "XML", "XSL"];
+return ["BSH", "C", "CC", "CPP", "CS", "CSS", "CSH", "CYC", "CV", "HTM", "HTML", "JAVA", "JS", "M", "MXML", "PERL", "PL", "PM", "PY", "RB", "SH", "XHTML", "XML", "XSL"];

5efabcae0d5d5f7ca0681a8003ae27c93753acb9
lDucks <justin@battleplugins.com>
2015-06-23 21:43:39 -0400

    Remove md

--- config/paste.php
+++ config/paste.php
@@ -2,4 +2,4 @@
 
 return ["BSH", "C", "CC", "CPP", "CS", "CSS", "CSH", "CYC", "CV", "HTM", "HTML",
     "JAVA", "JS", "M", "MXML", "PERL", "PL", "PM", "PY", "RB", "SH",
-    "XHTML", "XML", "XSL", "MD"];
+    "XHTML", "XML", "XSL"];

9ea928502a3f579555a1fdebfeb43891deb32bfe
lDucks <justin@battleplugins.com>
2015-06-23 21:58:05 -0400

    Don't have padding when no language is selected

--- resources/views/paste/paste.blade.php
+++ resources/views/paste/paste.blade.php
@@ -22,12 +22,14 @@
         @include('paste.partials.actions')
         <div class="grid-100">
             <small>{{ strlen($content) }}/{{ env("PASTE_MAX_LEN", 500000) }} characters. {{ $lines }} lines.</small>
-            <div class="paste">
-                @if($lang != 'auto')
+            @if($lang != 'auto')
+                <div class="paste">
                     <div class="ui top attached blue label">{{ $lang }}</div>
-                @endif
-                <pre class="prettyprint linenums lang-{{ $lang }}">{{ $content }}</pre>
-            </div>
+                    @endif
+                    <pre class="prettyprint linenums lang-{{ $lang }}">{{ $content }}</pre>
+                    @if($lang != 'auto')
+                </div>
+            @endif
             <small>{{ strlen($content) }}/{{ env("PASTE_MAX_LEN", 500000) }} characters. {{ $lines }} lines.</small>
         </div>
         @include('paste.partials.actions')

abb94555bc85333699bc658c4d138c05d21534f4
lDucks <justin@battleplugins.com>
2015-06-23 21:54:20 -0400

    Use more specific selectors

--- resources/views/paste/index.blade.php
+++ resources/views/paste/index.blade.php
@@ -9,7 +9,7 @@
                     </div>
                 </div>
             </div>
-            <div class="content grid-100">
+            <div class="paste content grid-100">
                 @if(session()->has('error'))
                     <div class="ui message negative">
                         {{ session()->get('error') }}

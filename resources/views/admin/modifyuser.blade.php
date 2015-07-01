d66f4c38ec3f61f4c689a417cc3b1556330bdf86
Justin <justin@battleplugins.com>
2015-07-01 16:57:46 -0400

    Add user table - will add functions later

--- resources/views/admin/modifyuser.blade.php
+++ resources/views/admin/modifyuser.blade.php
@@ -4,6 +4,23 @@
         <h2>Modify Users</h2>
     </div>
     <div class="grid-100">
-
+        <table class="ui celled striped table">
+            <thead>
+            <tr>
+                <th>Email</th>
+                <th>Display Name</th>
+                <th colspan="3">User Settings</th>
+            </tr>
+            </thead>
+            <tbody>
+            @foreach($users as $user)
+                <td>{{ $user->email }}</td>
+                <td>{{ $user->displayname }}</td>
+                <td></td>
+                <td></td>
+                <td></td>
+            @endforeach
+            </tbody>
+        </table>
     </div>
 @stop

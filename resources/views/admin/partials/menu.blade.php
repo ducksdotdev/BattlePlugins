b10714ccc0369148f3cbca2eb155222b73b8d102
Justin <justin@battleplugins.com>
2015-07-01 14:33:43 -0400

    Change mobile nav

--- resources/views/admin/partials/menu.blade.php
+++ resources/views/admin/partials/menu.blade.php
@@ -1,6 +1,28 @@
 <div class="ui vertical inverted admin menu hide-on-mobile">
-    @include('admin.partials.menuitems')
+    <a href="/" class="item">Home</a>
+    <div class="item">
+        <div class="header">CMS Solutions</div>
+        <div class="menu">
+            <a class="item">Rails</a>
+            <a class="item">Python</a>
+            <a class="item">PHP</a>
+        </div>
+    </div>
+    <div class="item">
+        <div class="header">Hosting</div>
+        <div class="menu">
+            <a class="item">Shared</a>
+            <a class="item">Dedicated</a>
+        </div>
+    </div>
+    <div class="item">
+        <div class="header">Support</div>
+        <div class="menu">
+            <a class="item">E-mail Support</a>
+            <a class="item">FAQs</a>
+        </div>
+    </div>
 </div>
 <div class="ui secondary pointing menu hide-on-desktop hide-on-tablet">
-    @include('admin.partials.menuitems')
+    <a href="/" class="item">Home</a>
 </div>

1b0ce23932aeb602c55ca9c30069166ec6c932cf
Justin <justin@battleplugins.com>
2015-07-01 16:37:03 -0400

    Import the proper registrar

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -2,8 +2,8 @@
 
 namespace App\Http\Controllers;
 
-use App\Services\Registrar;
 use Auth;
+use Illuminate\Contracts\Auth\Registrar;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Hash;
 

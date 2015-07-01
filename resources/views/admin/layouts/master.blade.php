dfca1ad4baf7aa1f7d8f433b58511d0e98c0c664
Justin <justin@battleplugins.com>
2015-07-01 12:42:22 -0400

    Extend proper layout

--- resources/views/admin/layouts/master.blade.php
+++ resources/views/admin/layouts/master.blade.php
@@ -8,6 +8,7 @@
 
     <!--        Styles -->
     <link rel="stylesheet" href="/assets/css/semantic.css">
+    <link rel="stylesheet" href="//cdnjs.cloudflare.com/ajax/libs/semantic-ui/1.12.0/components/icon.min.css" />
     <link rel="stylesheet" href="//cdnjs.cloudflare.com/ajax/libs/unsemantic/0/unsemantic-grid-responsive.css">
     <link rel="stylesheet" href="/assets/css/styles.css" type="text/css"/>
     <!--        End Styles -->

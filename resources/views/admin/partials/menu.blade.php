d58088147081e07a75d4b1288522f19df8474ac3
Justin <justin@battleplugins.com>
2015-07-01 13:27:17 -0400

    Add menu

--- resources/views/admin/partials/menu.blade.php
+++ resources/views/admin/partials/menu.blade.php
@@ -1,5 +1,14 @@
-<div class="ui left fixed inverted vertical menu">
-    <a class="active item">Home</a>
-    <a class="item">Messages</a>
-    <a class="item">Friends</a>
+<div class="ui left fixed vertical inverted menu">
+    <a class="item">Home</a>
+
+    <div class="ui dropdown item">
+        User Management
+        <i class="dropdown icon"></i>
+
+        <div class="menu">
+            <a class="item"><i class="edit icon"></i> Create User</a>
+            <a class="item"><i class="globe icon"></i> Manage Users</a>
+            <a class="item"><i class="settings icon"></i> Account Settings</a>
+        </div>
+    </div>
 </div>

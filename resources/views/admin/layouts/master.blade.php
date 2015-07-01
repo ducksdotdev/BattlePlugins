fc5627ec7b86145bff79608f3f6429cfd8749fbe
Justin <justin@battleplugins.com>
2015-07-01 12:37:13 -0400

    Add pusher element

---
+++ resources/views/admin/layouts/master.blade.php
@@ -0,0 +1,46 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>BattleAdmin :: BattlePlugins Administration Panel</title>
+    <link rel="icon" href="/assets/img/bp.png"/>
+
+    <!--        Styles -->
+    <link rel="stylesheet" href="/assets/css/semantic.css">
+    <link rel="stylesheet" href="//cdnjs.cloudflare.com/ajax/libs/unsemantic/0/unsemantic-grid-responsive.css">
+    <link rel="stylesheet" href="/assets/css/styles.css" type="text/css"/>
+    <!--        End Styles -->
+    <!--        Scripts -->
+    <script type="text/javascript" src="//cdnjs.cloudflare.com/ajax/libs/jquery/2.1.4/jquery.min.js"></script>
+    <script type="text/javascript" src="//cdnjs.cloudflare.com/ajax/libs/semantic-ui/1.12.2/semantic.min.js"></script>
+    <!--       End Scripts -->
+</head>
+<body>
+<nav>
+    <div class="grid-container">
+        <div class="grid-100">
+            <h1 class="brand"><a href="/">battleadmin</a></h1>
+        </div>
+    </div>
+</nav>
+<div class="ui left vertical visible inverted labeled icon sidebar menu">
+    <a class="item">
+        <i class="home icon"></i>
+        Home
+    </a>
+    <a class="item">
+        <i class="block layout icon"></i>
+        Topics
+    </a>
+    <a class="item">
+        <i class="smile icon"></i>
+        Friends
+    </a>
+</div>
+<div class="pusher">
+    @yield('content')
+</div>
+@include('footer')
+</body>
+</html>

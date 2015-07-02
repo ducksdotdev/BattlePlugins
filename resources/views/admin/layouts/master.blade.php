a32e0c47bc30da7786bde9af0bc89d5932b55086
Justin <justin@battleplugins.com>
2015-07-02 16:35:26 -0400

    Split alert view

--- resources/views/admin/layouts/master.blade.php
+++ resources/views/admin/layouts/master.blade.php
@@ -26,29 +26,7 @@
 <div class="grid-100 grid-parent">
     @include('admin.partials.menu')
     <div class="grid-100 grid-parent">
-        @if(count($alerts) > 0)
-            <div class="grid-100 grid-parent alerts">
-                <div class="grid-85 pull-right" ng-controller="AlertsCtrl">
-                    <table class="updates">
-                        <tbody>
-                        <tr>
-                            <td width="10%" class="text-center"><h3>Alerts: </h3></td>
-                            <td width="80%" ng-bind="alert['content']"></td>
-                            <td width="10%" class="text-center">
-                                <i ng-click="prevAlert()" ng-hide="alerts.length == 1"
-                                   class="icon caret left pointer"></i>
-                                <a id="removeAlert"><i class="icon remove pointer"></i></a>
-                                <i ng-click="nextAlert()" ng-hide="alerts.length == 1"
-                                   class="icon caret right pointer"></i>
-                            </td>
-                        </tr>
-                        </tbody>
-                    </table>
-                </div>
-            </div>
-        @elseif($alert_bar)
-            <div class="grid-100 grid-parent alerts"><div class="updates"></div></div>
-        @endif
+        @include('admin.partials.alerts')
         <div class="grid-85 pull-right grid-parent">
             <div class="grid-container">
                 @yield('content')

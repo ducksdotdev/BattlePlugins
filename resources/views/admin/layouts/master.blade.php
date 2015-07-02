882a7fd7accd52330d9e5b81c488e2649a27aa23
lDucks <justin@battleplugins.com>
2015-07-02 15:55:13 -0400

    Remove the white line from spacing issues

--- resources/views/admin/layouts/master.blade.php
+++ resources/views/admin/layouts/master.blade.php
@@ -25,26 +25,32 @@
 <body>
 <div class="grid-100 grid-parent">
     @include('admin.partials.menu')
-    <div class="grid-85 grid-parent pull-right">
-        @if(count($alerts) > 0)
-            <div class="grid-100 grid-parent alerts" ng-controller="AlertsCtrl">
-                <table class="updates">
-                    <tbody>
-                    <tr>
-                        <td width="10%" class="text-center"><h3>Alerts | </h3></td>
-                        <td width="80%" ng-bind="alert['content']"></td>
-                        <td width="10%" class="text-right">
-                            <i ng-click="prevAlert()" ng-hide="alerts.length == 1" class="icon caret left pointer"></i>
-                            <a id="removeAlert"><i class="icon remove pointer"></i></a>
-                            <i ng-click="nextAlert()" ng-hide="alerts.length == 1" class="icon caret right pointer"></i>
-                        </td>
-                    </tr>
-                    </tbody>
-                </table>
-            </div>
-        @else
-            <div class="grid-60 updates text-center">Hello, {{ auth()->user()->displayname }}. Welcome to BattleAdmin!</div>
-        @endif
+    <div class="grid-100 grid-parent pull-right">
+        <div class="grid-85 pull-right">
+            @if(count($alerts) > 0)
+                <div class="grid-100 grid-parent alerts" ng-controller="AlertsCtrl">
+                    <table class="updates">
+                        <tbody>
+                        <tr>
+                            <td width="10%" class="text-center"><h3>Alerts | </h3></td>
+                            <td width="80%" ng-bind="alert['content']"></td>
+                            <td width="10%" class="text-right">
+                                <i ng-click="prevAlert()" ng-hide="alerts.length == 1"
+                                   class="icon caret left pointer"></i>
+                                <a id="removeAlert"><i class="icon remove pointer"></i></a>
+                                <i ng-click="nextAlert()" ng-hide="alerts.length == 1"
+                                   class="icon caret right pointer"></i>
+                            </td>
+                        </tr>
+                        </tbody>
+                    </table>
+                </div>
+            @else
+                <div class="grid-60 updates text-center">Hello, {{ auth()->user()->displayname }}. Welcome to
+                    BattleAdmin!
+                </div>
+            @endif
+        </div>
         <div class="grid-container">
             @yield('content')
         </div>

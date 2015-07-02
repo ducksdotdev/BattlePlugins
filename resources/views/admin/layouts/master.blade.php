41dba4655b72fd0155d892377037ffab17e65ef1
lDucks <justin@battleplugins.com>
2015-07-02 16:01:06 -0400

    Move grid around

--- resources/views/admin/layouts/master.blade.php
+++ resources/views/admin/layouts/master.blade.php
@@ -26,24 +26,24 @@
 <div class="grid-100 grid-parent">
     @include('admin.partials.menu')
     <div class="grid-100 grid-parent pull-right">
-        <div class="grid-85 pull-right">
+        <div class="grid-100 grid-parent alerts">
             @if(count($alerts) > 0)
-                <div class="grid-100 grid-parent alerts" ng-controller="AlertsCtrl">
-                    <table class="updates">
-                        <tbody>
-                        <tr>
-                            <td width="10%" class="text-center"><h3>Alerts | </h3></td>
-                            <td width="80%" ng-bind="alert['content']"></td>
-                            <td width="10%" class="text-right">
-                                <i ng-click="prevAlert()" ng-hide="alerts.length == 1"
-                                   class="icon caret left pointer"></i>
-                                <a id="removeAlert"><i class="icon remove pointer"></i></a>
-                                <i ng-click="nextAlert()" ng-hide="alerts.length == 1"
-                                   class="icon caret right pointer"></i>
-                            </td>
-                        </tr>
-                        </tbody>
-                    </table>
+                <div class="grid-85 pull-right" ng-controller="AlertsCtrl">
+                        <table class="updates">
+                            <tbody>
+                            <tr>
+                                <td width="10%" class="text-center"><h3>Alerts: </h3></td>
+                                <td width="80%" ng-bind="alert['content']"></td>
+                                <td width="10%" class="text-center">
+                                    <i ng-click="prevAlert()" ng-hide="alerts.length == 1"
+                                       class="icon caret left pointer"></i>
+                                    <a id="removeAlert"><i class="icon remove pointer"></i></a>
+                                    <i ng-click="nextAlert()" ng-hide="alerts.length == 1"
+                                       class="icon caret right pointer"></i>
+                                </td>
+                            </tr>
+                            </tbody>
+                        </table>
                 </div>
             @else
                 <div class="grid-60 updates text-center">Hello, {{ auth()->user()->displayname }}. Welcome to
@@ -51,8 +51,10 @@
                 </div>
             @endif
         </div>
-        <div class="grid-container">
-            @yield('content')
+        <div class="grid-85 pull-right grid-parent">
+            <div class="grid-container">
+                @yield('content')
+            </div>
         </div>
         @include('footer')
     </div>

96f0fe78619021f2663b85b21f8336e7f7ba8844
lDucks <justin@battleplugins.com>
2015-07-01 21:11:34 -0400

    Separate Buttons

--- resources/views/admin/cms.blade.php
+++ resources/views/admin/cms.blade.php
@@ -2,12 +2,9 @@
 @section('content')
     <div class="grid-100">
         <h2>{{ $title }}</h2>
-        @if($jenkins)
-            <a href="/tools/cms/jenkins" class="ui button red">Disable Jenkins Feed</a>
-        @else
-            <a href="/tools/cms/jenkins" class="ui button primary">Enable Jenkins Feed</a>
-        @endif
-
+    </div>
+    <div class="grid-50">
+        <h3>All Sites</h3>
         @if($registration)
             <a href="/tools/cms/registration" class="ui button red">Disable Registration</a>
         @else
@@ -18,6 +15,14 @@
             <a href="/tools/cms/footer" class="ui button red">Disable Footer</a>
         @else
             <a href="/tools/cms/footer" class="ui button primary">Enable Footer</a>
+        @endif
+    </div>
+    <div class="grid-50">
+        <h3>Blog</h3>
+        @if($jenkins)
+            <a href="/tools/cms/jenkins" class="ui button red">Disable Jenkins Feed</a>
+        @else
+            <a href="/tools/cms/jenkins" class="ui button primary">Enable Jenkins Feed</a>
         @endif
     </div>
 @stop

28e406a4ffc53cb8c0baa1718da168742bb7b377
lDucks <justin@battleplugins.com>
2015-07-04 18:02:27 -0400

    Color code based on number of tasks

--- resources/views/admin/partials/dashboard/tasks.blade.php
+++ resources/views/admin/partials/dashboard/tasks.blade.php
@@ -1,18 +1,18 @@
 <h3>Tasks Overview <a href="{{ action('Tasks\PageController@index') }}"><i class="icon external"></i></a></h3>
 <ul class="stats">
-    <li class="yellow has-small">
+    <li class="{{ count($tasks->where('assigned_to', auth()->user()->id)->where('status', false)->get()) ? 'yellow' : 'green' }} has-small">
         {{ count($tasks->where('assigned_to', auth()->user()->id)->where('status', false)->get()) }}
         <div class="small">Assigned to You</div>
     </li>
-    <li class="yellow has-small">
+    <li class="{{ count($tasks->where('status', false)->get()) ? 'yellow' : 'green' }} has-small">
         {{ count($tasks->where('status', false)->get()) }}
         <div class="small">Incomplete</div>
     </li>
-    <li class="red has-small">
+    <li class="{{ count($tasks->where('status', false)->where('creator', 24)->get()) ? 'red' : 'green' }} has-small">
         {{ count($tasks->where('status', false)->where('creator', 24)->get()) }}
         <div class="small">GitHub Issues</div>
     </li>
-    <li class="green has-small">
+    <li class="{{ count($tasks->where('status', true)->get()) ? 'green' : 'red'}} has-small">
         {{ count($tasks->where('status', true)->get()) }}
         <div class="small">Completed</div>
     </li>

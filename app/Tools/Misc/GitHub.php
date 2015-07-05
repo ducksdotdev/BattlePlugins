ee1f3f5187482e9211ac2953d54eaedb85a7c285
lDucks <justin@battleplugins.com>
2015-07-05 01:47:45 -0400

    Remove DD

--- app/Tools/Misc/GitHub.php
+++ app/Tools/Misc/GitHub.php
@@ -20,8 +20,6 @@ class GitHub {
         ];
         $url = $url . '?' . http_build_query($params);
 
-        dd($url);
-
         $ch = curl_init();
         curl_setopt($ch, CURLOPT_URL, $url);
         curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);

4afab17885ae34398504d2109f7cabf436076ef5
lDucks <justin@battleplugins.com>
2015-07-05 23:22:53 -0400

    Some minor changes to the github feed

--- resources/views/admin/github.blade.php
+++ resources/views/admin/github.blade.php
@@ -2,7 +2,7 @@
 @section('content')
     <div class="grid-70 grid-parent">
         <div class="grid-100">
-            <h3>Events Feed</h3>
+            <h3>Events Feed (Last 100 events)</h3>
         </div>
         <div class="grid-100">
             @include('admin.partials.github')

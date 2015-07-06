ee349a07d2c9d2dcdf60fbff248e546cab22b1a8
Justin <justin@battleplugins.com>
2015-07-06 16:22:00 -0400

    Make mobile grid 75

--- resources/views/admin/partials/menu.blade.php
+++ resources/views/admin/partials/menu.blade.php
@@ -1,9 +1,9 @@
-<div class="grid-20 mobile-grid-50 tablet-grid-30 admin brand">
+<div class="grid-20 mobile-grid-75 tablet-grid-30 admin brand">
     <h1>
         <i id="closeMenu" class="icon sidebar pointer hide-on-desktop"></i>
         <a href="/">battleadmin</a>
     </h1>
 </div>
-<div class="grid-20 mobile-grid-50 tablet-grid-30 ui vertical inverted admin menu left fixed">
+<div class="grid-20 mobile-grid-75 tablet-grid-30 ui vertical inverted admin menu left fixed">
     @include('admin.partials.menuitems')
 </div>

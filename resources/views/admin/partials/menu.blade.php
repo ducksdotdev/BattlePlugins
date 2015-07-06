92bceec4e143734891e1baec81e7cc43890a8966
lDucks <justin@battleplugins.com>
2015-07-06 17:42:49 -0400

    Use wide on mobile

--- resources/views/admin/partials/menu.blade.php
+++ resources/views/admin/partials/menu.blade.php
@@ -1,6 +1,6 @@
 <div class="grid-20 ui left vertical inverted sidebar admin menu visible hide-on-mobile hide-on-tablet">
     @include('admin.partials.menuitems')
 </div>
-<div id="mobileMenu" class="ui left vertical inverted normal sidebar admin menu hide-on-desktop">
+<div id="mobileMenu" class="ui left vertical inverted wide sidebar admin menu hide-on-desktop">
     @include('admin.partials.menuitems')
 </div>

c24dc44a49dd57840f257834fb68c53ec00f04e8
Justin <justin@battleplugins.com>
2015-07-06 16:47:47 -0400

    Remove grid from mobile

--- resources/views/admin/partials/menu.blade.php
+++ resources/views/admin/partials/menu.blade.php
@@ -1,6 +1,6 @@
-<div class="grid-20 mobile-grid-75 tablet-grid-30 ui left vertical inverted sidebar admin menu visible hide-on-mobile hide-on-tablet">
+<div class="grid-20 ui left vertical inverted sidebar admin menu visible hide-on-mobile hide-on-tablet">
     @include('admin.partials.menuitems')
 </div>
-<div id="mobileMenu" class="grid-20 mobile-grid-75 tablet-grid-30 ui left vertical inverted sidebar admin menu hide-on-desktop">
+<div id="mobileMenu" class="ui left vertical inverted sidebar admin menu hide-on-desktop">
     @include('admin.partials.menuitems')
 </div>

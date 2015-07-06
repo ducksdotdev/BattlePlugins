2f3c3ef58eb10a899032c10ef383d06d331bd232
Justin <justin@battleplugins.com>
2015-07-06 15:51:41 -0400

    Change colors

--- resources/views/admin/partials/dashboard/overview.blade.php
+++ resources/views/admin/partials/dashboard/overview.blade.php
@@ -1,10 +1,18 @@
 <h3>General Overview</h3>
 <ul class="stats">
     <li class="has-small">
+        {{ $pastes }}
+        <div class="small">Pastes</div>
+    </li>
+    <li class="has-small">
+        {{ $urls }}
+        <div class="small">Short URLs</div>
+    </li>
+    <li class="has-small">
         {{ count($blogs) }}
         <div class="small">Blog Posts</div>
     </li>
-    <li class="has-small">
+    <li class="blue has-small">
         {{ $hits  }}
         @if($hitChange > 0)
             <small class="green">(+{{ $hitChange }})</small>
@@ -12,13 +20,5 @@
             <small>({{ $hitChange }})</small>
         @endif
         <div class="small">Blog Hits</div>
-    </li>
-    <li class="blue has-small">
-        {{ $pastes }}
-        <div class="small">Pastes</div>
-    </li>
-    <li class="blue has-small">
-        {{ $urls }}
-        <div class="small">Short URLs</div>
     </li>
 </ul>

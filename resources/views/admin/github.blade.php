15641147ff87407ee7626a425857ed1eef298f8d
Justin <justin@battleplugins.com>
2015-07-06 13:11:02 -0400

    Keep grid on mobile

--- resources/views/admin/github.blade.php
+++ resources/views/admin/github.blade.php
@@ -14,7 +14,7 @@
         </div>
         <div class="grid-100 grid-parent">
             @foreach($members as $member)
-                <div class="grid-50 bottom-margin ten">
+                <div class="grid-50 mobile-grid-50 tablet-grid-50 bottom-margin ten">
                     <div class="ui segment text-center">
                         <div class="ui avatar big image">
                             <a href="{{ $member->html_url }}"><img src="{{ $member->avatar_url }}"></a>
@@ -32,7 +32,7 @@
         <div class="grid-100">
             <div class="ui segment">
                 @foreach($repos as $repo)
-                    <div class="grid-50">
+                    <div class="grid-50 mobile-grid-50 tablet-grid-50">
                         <div class="content">
                             <a class="header" href="{{ $repo->html_url }}">{{ $repo->name }}</a>
                         </div>

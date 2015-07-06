0b5489e01775b882df786d5acbaef5b56bd77194
Justin <justin@battleplugins.com>
2015-07-06 16:29:47 -0400

    Remove hiding

--- resources/views/admin/partials/menu.blade.php
+++ resources/views/admin/partials/menu.blade.php
@@ -1,3 +1,37 @@
-<div class="grid-20 mobile-grid-75 tablet-grid-30 ui vertical inverted admin menu left fixed hide-on-mobile hide-on-tablet">
-    @include('admin.partials.menuitems')
+<div class="grid-20 mobile-grid-75 tablet-grid-30 ui vertical inverted admin menu left fixed">
+    <div class="brand item">
+        <h1>
+            <i id="closeMenu" class="icon sidebar pointer hide-on-desktop"></i>
+            <a href="/">battleadmin</a>
+        </h1>
+    </div>
+
+    <a href="/" class="item">Dashboard</a>
+
+    @if(Auth::user()->admin)
+        <div class="item">
+            <div class="header">User Management</div>
+            <div class="menu">
+                <a href="/user/create" class="item">Create User</a>
+                <a href="/user/modify" class="item">Modify User</a>
+            </div>
+        </div>
+    @endif
+    <div class="item">
+        <div class="header">Tools</div>
+        <div class="menu">
+            @if(Auth::user()->admin)
+                <a href="/tools/alert" class="item">Create Alert</a>
+            @endif
+            <a href="/tools/cms" class="item">Manage Content</a>
+        </div>
+    </div>
+    <div class="item">
+        <div class="header"><img src="{{ $avatar }}" class="ui mini avatar image"> {{ auth()->user()->displayname}}
+        </div>
+        <div class="menu">
+            <a href="/settings" class="item">Settings</a>
+            <a href="/logout" class="item">Logout</a>
+        </div>
+    </div>
 </div>

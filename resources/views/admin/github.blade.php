6a1e4a42937a220ec0072c897b1b8dfceede613b
lDucks <justin@battleplugins.com>
2015-07-05 22:36:05 -0400

    Use bigger images for avatars on member list

--- resources/views/admin/github.blade.php
+++ resources/views/admin/github.blade.php
@@ -5,31 +5,7 @@
             <h3>Events Feed</h3>
         </div>
         <div class="grid-100">
-            @foreach($github as $item)
-                <div class="ui feed segment">
-                    <div class="event">
-                        <div class="label">
-                            <a href="http://github.com/{{ $item->actor->login }}">
-                                <img src="{{ $item->actor->avatar_url }}">
-                            </a>
-                        </div>
-                        <div class="content">
-                            <div class="date">
-                                {{ (new \Carbon\Carbon($item->created_at))->diffForHumans() }}
-                            </div>
-                            <div class="summary">
-                                @if($item->type == 'PushEvent')
-                                    @include('admin.partials.dashboard.gittypes.push')
-                                @elseif($item->type == 'IssueCommentEvent')
-                                    @include('admin.partials.dashboard.gittypes.issuecomment')
-                                @elseif($item->type == 'IssuesEvent')
-                                    @include('admin.partials.dashboard.gittypes.issues')
-                                @endif
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            @endforeach
+            @include('admin.partials.github')
         </div>
     </div>
     <div class="grid-30 grid-parent">
@@ -40,7 +16,7 @@
             @foreach($members as $member)
                 <div class="grid-50 bottom-margin ten">
                     <div class="ui segment text-center">
-                        <div class="ui avatar image">
+                        <div class="ui avatar big image">
                             <a href="{{ $member->html_url }}"><img src="{{ $member->avatar_url }}"></a>
                         </div>
                         <div class="content">
---
+++ resources/views/admin/partials/github.blade.php
@@ -0,0 +1,25 @@
+@foreach($github as $item)
+    <div class="ui feed segment">
+        <div class="event">
+            <div class="label">
+                <a href="http://github.com/{{ $item->actor->login }}">
+                    <img src="{{ $item->actor->avatar_url }}">
+                </a>
+            </div>
+            <div class="content">
+                <div class="date">
+                    {{ (new \Carbon\Carbon($item->created_at))->diffForHumans() }}
+                </div>
+                <div class="summary">
+                    @if($item->type == 'PushEvent')
+                        @include('admin.partials.dashboard.gittypes.push')
+                    @elseif($item->type == 'IssueCommentEvent')
+                        @include('admin.partials.dashboard.gittypes.issuecomment')
+                    @elseif($item->type == 'IssuesEvent')
+                        @include('admin.partials.dashboard.gittypes.issues')
+                    @endif
+                </div>
+            </div>
+        </div>
+    </div>
+@endforeach

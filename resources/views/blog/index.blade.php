8c91f18976bb5c04c8aee2535ea6cf708ca29481
lDucks <justin@battleplugins.com>
2015-07-06 17:32:56 -0400

    Colors to index

--- resources/views/blog/index.blade.php
+++ resources/views/blog/index.blade.php
@@ -2,6 +2,7 @@
 <html lang="en">
 <head>
     <meta charset="utf-8">
+    @include('mobilecolor')
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>BattlePlugins :: Minecraft Plugin Development Team</title>
     <link rel="icon" href="/assets/img/bp.png"/>

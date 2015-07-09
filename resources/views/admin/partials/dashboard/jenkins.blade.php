6fec43d9554433a112ca9f282c2d856c8dbe0b4e
lDucks <justin@battleplugins.com>
2015-07-09 18:16:28 -0400

    Aborted

--- resources/views/admin/partials/dashboard/jenkins.blade.php
+++ resources/views/admin/partials/dashboard/jenkins.blade.php
@@ -4,7 +4,7 @@
 @if($rssFeed)
     @foreach($rssFeed as $item)
         <div class="grid-100">
-            <div class="ui small message {{ str_contains($item->get_title(), 'broken') ? 'red' : 'green' }}">
+            <div class="ui small message {{ str_contains($item->get_title(), ['broken','aborted']) ? 'red' : 'green' }}">
                 <div class="header">
                     <a href="{{ $item->get_permalink()}}">{{ $item->get_title() }}</a>
                 </div>

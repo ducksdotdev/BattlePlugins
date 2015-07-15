c2a845461c522781f3127053b2dc56c9df905d61
lDucks <justin@battleplugins.com>
2015-07-14 23:20:58 -0400

    Change to environment variables

--- app/Http/Controllers/Voice/PageController.php
+++ app/Http/Controllers/Voice/PageController.php
@@ -9,10 +9,10 @@ class PageController extends Controller {
     public function index() {
         return view('voice.index');
     }
-    
+
     public function feed() {
 
-        $ts3 = TeamSpeak3::factory("serverquery://voice:5Z0dXOXf@23.226.234.128:10011/?server_port=9987");
+        $ts3 = TeamSpeak3::factory("serverquery://" . env("TS_USER") . ":" . env("TS_PASS") . "@" . env('TS_URL', 'localhost') . ":10011/?server_port=9987");
         $ts3 = $ts3->getViewer(new TeamSpeak3_Viewer_Html("assets/img/ts/viewer/", "assets/img/ts/flags/", "data:image"));
 
         return $ts3;

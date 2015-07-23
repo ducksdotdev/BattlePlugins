35977b9748c8e4ac89e414cd73ed9bc8680d1d2a
lDucks <justin@battleplugins.com>
2015-07-23 16:02:10 -0400

    Completed class

--- resources/views/tasks/partials/tasks.blade.php
+++ resources/views/tasks/partials/tasks.blade.php
@@ -1,16 +1,16 @@
 <div class="grid-container">
     <div ng-controller="TasksCtrl">
         <div class="grid-100">
+            @include('tasks.partials.header')
+            @include('tasks.partials.tasklist')
+        </div>
+        <div class="grid-100">
             <div class="field">
                 <div class="ui toggle checkbox" ng-click="toggleCompleted()">
                     <input type="checkbox">
                     <label>Show completed tasks?</label>
                 </div>
             </div>
-        </div>
-        <div class="grid-100">
-            @include('tasks.partials.header')
-            @include('tasks.partials.tasklist')
         </div>
     </div>
     <div class="grid-100">

f7a1723d2272869d788d51ee602aaa84740f4571
lDucks <justin@battleplugins.com>
2015-07-23 13:05:19 -0400

    Add WatchEvent for GitHub

---
+++ resources/views/admin/partials/dashboard/gittypes/WatchEvent.blade.php
@@ -0,0 +1,2 @@
+<a href="http://github.com/{{ $item->actor->login }}">{{ $item->actor->login }}</a> watched repo <a
+        href="https://github.com/{{ $item->repo->name }}">{{ $item->repo->name  }}</a>.

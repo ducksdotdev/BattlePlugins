7db47162cd405ec76ffac933b5fdc14d96143f4c
lDucks <justin@battleplugins.com>
2015-07-24 16:40:22 -0400

    Add Jenkins check

--- resources/views/admin/partials/dashboard/jenkins.blade.php
+++ resources/views/admin/partials/dashboard/jenkins.blade.php
@@ -1,29 +1,31 @@
-<div class="grid-100">
-    <h3>Latest Jenkins Builds <a href="http://ci.battleplugins.com"><i class="icon external"></i></a></h3>
-</div>
-@if(!$jenkins_online)
+@if(count($jenkins))
     <div class="grid-100">
-        <div class="ui message negative text-center">
-            <h2>The Jenkins Server is OFFLINE!</h2>
-        </div>
+        <h3>Latest Jenkins Builds <a href="http://ci.battleplugins.com"><i class="icon external"></i></a></h3>
     </div>
-@else
-    @foreach($jenkins as $build)
+    @if(!$jenkins_online)
         <div class="grid-100">
-            <div class="ui small message {{ $build->result == 'SUCCESS' ? 'green' : 'red' }}">
-                <a href="{{ $build->url }}">{{ $build->fullDisplayName }} -
+            <div class="ui message negative text-center">
+                <h2>The Jenkins Server is OFFLINE!</h2>
+            </div>
+        </div>
+    @else
+        @foreach($jenkins as $build)
+            <div class="grid-100">
+                <div class="ui small message {{ $build->result == 'SUCCESS' ? 'green' : 'red' }}">
+                    <a href="{{ $build->url }}">{{ $build->fullDisplayName }} -
                     <span title="{{ Carbon::createFromTimestampUTC(str_limit($build->timestamp, 10)) }}">
                         {{ Carbon::createFromTimestampUTC(str_limit($build->timestamp, 10))->diffForHumans() }}
                     </span>
-                </a>
-                @if(Jenkins::getJobs(Jenkins::getJobFromBuild($build))->healthReport)
-                    <p>
-                        @foreach(Jenkins::getJobs(Jenkins::getJobFromBuild($build))->healthReport as $report)
-                            {{ $report->description }}
-                        @endforeach
-                    </p>
-                @endif
+                    </a>
+                    @if(Jenkins::getJobs(Jenkins::getJobFromBuild($build))->healthReport)
+                        <p>
+                            @foreach(Jenkins::getJobs(Jenkins::getJobFromBuild($build))->healthReport as $report)
+                                {{ $report->description }}
+                            @endforeach
+                        </p>
+                    @endif
+                </div>
             </div>
-        </div>
-    @endforeach
+        @endforeach
+    @endif
 @endif

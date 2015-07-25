48184ef3f6c71aee474d2e17a24ffa1050d0f551
lDucks <justin@battleplugins.com>
2015-07-25 13:53:57 -0400

    Only show the log to an admin

--- resources/views/admin/partials/dashboard/log.blade.php
+++ resources/views/admin/partials/dashboard/log.blade.php
@@ -1,17 +1,19 @@
-<h3>Latest Log <a href="{{ action('Admin\PageController@logs') }}"><i class="icon external"></i></a></h3>
+@if(auth()->user()->admin)
+    <h3>Latest Log <a href="{{ action('Admin\PageController@logs') }}"><i class="icon external"></i></a></h3>
 
-<div class="ui message {{$log['level_class']}} log">
-    <strong>{{ ucfirst($log['level']) }} - {{ (new Carbon($log['date']))->diffForHumans() }}
-        ({{ $log['date'] }})</strong>
+    <div class="ui message {{$log['level_class']}} log">
+        <strong>{{ ucfirst($log['level']) }} - {{ (new Carbon($log['date']))->diffForHumans() }}
+            ({{ $log['date'] }})</strong>
 
-    <p>{{ $log['text'] }}
-        @if (isset($log['in_file']))
-            {{ $log['in_file'] }}
+        <p>{{ $log['text'] }}
+            @if (isset($log['in_file']))
+                {{ $log['in_file'] }}
+            @endif
+        </p>
+        @if ($log['stack'])
+            <a class="pointer" onclick="$('#stack').toggleClass('hidden')"><i
+                        class="icon archive"></i> Toggle Stack</a>
+            <pre id="stack" class="hidden">{!! trim($log['stack']) !!}</pre>
         @endif
-    </p>
-    @if ($log['stack'])
-        <a class="pointer" onclick="$('#stack').toggleClass('hidden')"><i
-                    class="icon archive"></i> Toggle Stack</a>
-        <pre id="stack" class="hidden">{!! trim($log['stack']) !!}</pre>
-    @endif
-</div>
+    </div>
+@endif

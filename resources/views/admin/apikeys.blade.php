cb6c729e05ff69b2e4ec9c147613db37291cd8bc
lDucks <justin@battleplugins.com>
2015-07-29 15:12:54 -0400

    Hide keys

--- resources/views/admin/apikeys.blade.php
+++ resources/views/admin/apikeys.blade.php
@@ -1,7 +1,7 @@
 @extends('admin.layouts.master')
 @section('content')
     <div class="grid-100">
-        <div class="ui message">
+        <div class="ui message warning">
             Please remember that these API keys would allow someone to make actions as a user. Do not give these keys out to anyone.
         </div>
     </div>
@@ -10,14 +10,14 @@
             <thead>
             <tr>
                 <th>User</th>
-                <th>Key</th>
+                <th>Key (Hover to view)</th>
             </tr>
             </thead>
             <tbody>
             @foreach($users as $user)
                 <tr>
                     <td>{{ $user->email }} - {{ $user->displayname }}</td>
-                    <td>{{ $user->api_key }}</td>
+                    <td><span class="spoiler">{{ $user->api_key }}</span></td>
                 </tr>
             @endforeach
             </tbody>

3798979bddb2b54fe2bcdb2cdb7638186f38227d
lDucks <justin@battleplugins.com>
2015-07-30 12:41:29 -0400

    Scroll

--- resources/views/admin/permissionoverview.blade.php
+++ resources/views/admin/permissionoverview.blade.php
@@ -1,17 +1,22 @@
 @extends('admin.layouts.master')
 @section('content')
     @foreach($nodes as $node)
-        <div class="grid-25">
-            <h3>{{ $node }}</h3>
-            @if(count($permissions->whereNode($node)->get()) > 0)
-                <ul>
-                    @foreach($permissions->whereNode($node)->get() as $perm)
-                        <li>{{ $perm->user->displayname }}</li>
-                    @endforeach
-                </ul>
-            @else
-                Empty Group
-            @endif
+        <div class="grid-25 bottom-margin ten">
+            <div class="ui segment">
+                <h3>{{ $node }}</h3>
+
+                <div style="height:150px; overflow:auto;">
+                    @if(count($permissions->whereNode($node)->get()) > 0)
+                        <ul>
+                            @foreach($permissions->whereNode($node)->get() as $perm)
+                                <li>{{ $perm->user->displayname }}</li>
+                            @endforeach
+                        </ul>
+                    @else
+                        Empty Group
+                    @endif
+                </div>
+            </div>
         </div>
     @endforeach
 @stop

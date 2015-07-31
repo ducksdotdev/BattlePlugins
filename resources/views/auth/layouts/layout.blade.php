c0e3c7e4d5f551baa4945d59604c3ee83ad09e34
lDucks <justin@battleplugins.com>
2015-07-30 21:43:50 -0400

    Icon styles

--- resources/views/auth/layouts/layout.blade.php
+++ resources/views/auth/layouts/layout.blade.php
@@ -10,6 +10,7 @@
     <link rel="stylesheet" href="/assets/css/semantic.css">
     <link rel="stylesheet" href="//cdnjs.cloudflare.com/ajax/libs/unsemantic/0/unsemantic-grid-responsive.css">
     <link rel="stylesheet" href="//cdnjs.cloudflare.com/ajax/libs/semantic-ui/1.12.2/components/accordion.min.css">
+    <link rel="stylesheet" href="//cdnjs.cloudflare.com/ajax/libs/semantic-ui/1.12.2/components/icon.min.css">
     <link rel="stylesheet" href="/assets/css/styles.css" type="text/css"/>
     <!--        End Styles -->
     <script type="text/javascript" src="//cdnjs.cloudflare.com/ajax/libs/jquery/2.1.4/jquery.min.js"></script>
